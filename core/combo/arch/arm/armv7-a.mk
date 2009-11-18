05b33a537f45831296bced7fe998081da92ee52c
Gary King <gking@nvidia.com>
2009-11-18 14:02:38 -0800

    armv7-a build: allow for board customization of CPU/FPU target
    to cover the entire gamut of CPU and FPU compilation types possible
    with ARMv7 (uniprocessor vs SMP, no-FPU vs VFPv3 vs Neon, A8 work-
    arounds required vs unnecessary), the compilation codepath needs to
    be specified by the BoardConfig.mk to ensure that the OS matches the
    target hardware.
    
    add TARGET_ARCH_VARIANT_FPU, TARGET_ARCH_VARIANT_CPU and
    TARGET_CPU_SMP makefile variables to allow for target-directed
    compilation flags.
    
    Change-Id: Ie968b3d9a4dff0020671725da2b5a6b42fec8a28

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -8,15 +8,42 @@ ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
 ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
+ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),none)
 ARCH_ARM_HAVE_VFP               := true
+else
+ARCH_ARM_HAVE_VFP               := false
+endif
 
-# Note: Hard coding the 'tune' value here is probably not ideal,
-# and a better solution should be found in the future.
-#
+ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
+TARGET_ARCH_VARIANT_FPU         := neon
+endif
+
+ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),neon)
+ARCH_ARM_HAVE_NEON              := true
+endif
+
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
+TARGET_ARCH_VARIANT_CPU         := cortex-a8
+endif
+
+ifeq ($(strip $(TARGET_CPU_SMP)),true)
+ARCH_ARM_HAVE_TLS_REGISTER      := true
+endif
+
 arch_variant_cflags := \
     -march=armv7-a \
-    -mfloat-abi=softfp \
-    -mfpu=vfpv3-d16
+    -mtune=$(TARGET_ARCH_VARIANT_CPU)
 
+ifeq ($(strip $(ARCH_ARM_HAVE_VFP)),true)
+arch_variant_cflags += -mfloat-abi=softfp
+arch_variant_cflags += -mfpu=$(TARGET_ARCH_VARIANT_FPU)
+else
+arch_variant_cflags += -mfloat-abi=soft
+endif
+
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a8)
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
+else
+arch_variant_ldflags :=
+endif
