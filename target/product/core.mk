01643f695052de43784b839e2b4e2da4d7bf80a7
Steve Kondik <shade@chemlab.org>
2010-03-27 15:33:25 -0400

    Add NO_DEFAULT_SOUNDS to override the default ringtones.

--- target/product/core.mk
+++ target/product/core.mk
@@ -2,9 +2,12 @@ PRODUCT_BRAND :=
 PRODUCT_NAME :=
 PRODUCT_DEVICE :=
 PRODUCT_POLICY := android.policy_phone
+
+ifneq ($(NO_DEFAULT_SOUNDS),true)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.notification_sound=OnTheHunt.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
+endif
 
 PRODUCT_PACKAGES := \
     framework-res \
