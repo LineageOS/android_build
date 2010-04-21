016d68983e3430d3d09aca85fdd795a8fdfd41a4
Steve Kondik <shade@chemlab.org>
2010-04-21 12:10:52 -0400

    Add PRODUCT_INFO_PREBUILT option.
    This allows us to skip auto-generation of certain values in build.prop
    in favor of including our own prebuilts. This can be used with CM's
    modified init to support multiple devices.

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -15,11 +15,13 @@ echo "ro.build.type=$TARGET_BUILD_TYPE"
 echo "ro.build.user=$USER"
 echo "ro.build.host=`hostname`"
 echo "ro.build.tags=$BUILD_VERSION_TAGS"
-echo "ro.product.model=$PRODUCT_MODEL"
-echo "ro.product.brand=$PRODUCT_BRAND"
-echo "ro.product.name=$PRODUCT_NAME"
-echo "ro.product.device=$TARGET_DEVICE"
-echo "ro.product.board=$TARGET_BOOTLOADER_BOARD_NAME"
+if [ "$PRODUCT_INFO_PREBUILT" != "true" ] ; then
+  echo "ro.product.model=$PRODUCT_MODEL"
+  echo "ro.product.brand=$PRODUCT_BRAND"
+  echo "ro.product.name=$PRODUCT_NAME"
+  echo "ro.product.device=$TARGET_DEVICE"
+  echo "ro.product.board=$TARGET_BOOTLOADER_BOARD_NAME"
+fi
 echo "ro.product.cpu.abi=$TARGET_CPU_ABI"
 if [ -n "$TARGET_CPU_ABI2" ] ; then
   echo "ro.product.cpu.abi2=$TARGET_CPU_ABI2"
@@ -30,8 +32,10 @@ echo "ro.product.locale.region=$PRODUCT_DEFAULT_REGION"
 echo "ro.wifi.channels=$PRODUCT_DEFAULT_WIFI_CHANNELS"
 echo "ro.board.platform=$TARGET_BOARD_PLATFORM"
 
-echo "# ro.build.product is obsolete; use ro.product.device"
-echo "ro.build.product=$TARGET_DEVICE"
+if [ "$PRODUCT_INFO_PREBUILT" != "true" ] ; then
+  echo "# ro.build.product is obsolete; use ro.product.device"
+  echo "ro.build.product=$TARGET_DEVICE"
+fi
 
 echo "# Do not try to parse ro.build.description or .fingerprint"
 echo "ro.build.description=$PRIVATE_BUILD_DESC"
