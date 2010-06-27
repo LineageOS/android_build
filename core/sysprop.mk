f5e57f1e34aded358519f3162306ba91f3dbd4b8
Koushik Dutta <koushd@gmail.com>
2010-06-26 22:53:55 -0700

    core: Add support for overriding build.prop settings
    Change-Id: Ie4224d608b7bcb0c2087bd77bc954f8f5f9b6495

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -307,6 +307,7 @@ $(gen_from_buildinfo_sh): $(INTERNAL_BUILD_ID_MAKEFILE) $(API_FINGERPRINT) | $(B
 	        TARGET_CPU_ABI="$(TARGET_CPU_ABI)" \
 	        TARGET_CPU_ABI2="$(TARGET_CPU_ABI2)" \
 	        ZYGOTE_FORCE_64_BIT="$(ZYGOTE_FORCE_64_BIT)" \
+	        $(PRODUCT_BUILD_PROP_OVERRIDES) \
 	        bash $(BUILDINFO_SH) > $@
 
 ifdef TARGET_SYSTEM_PROP
