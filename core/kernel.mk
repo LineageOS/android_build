4ae302db0d28ad812608cd0522c0b3ec53604519
Chris Soyars <ctsoyars@gmail.com>
2010-07-07 07:54:37 +0000

    Remove circular dependency.

--- core/kernel.mk
+++ core/kernel.mk
@@ -84,9 +84,6 @@ kernel: $(TARGET_PREBUILT_KERNEL)
 
 else
 
-$(TARGET_PREBUILT_KERNEL): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
-
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	@echo "Transforming $(INSTALLED_KERNEL_TARGET) to $(TARGET_PREBUILT_KERNEL)"
 	$(transform-prebuilt-to-target)
