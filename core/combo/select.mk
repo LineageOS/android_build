608402901a268c7db89f1dbbc06bd3d915710ad7
Steve Kondik <shade@chemlab.org>
2010-08-08 16:25:26 -0400

    HACK: Set USE_ECLAIR_MEMORYDEALER for Eclair compatibility.

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -61,6 +61,10 @@ else
 $(combo_target)PRELINKER_MAP := $(BUILD_SYSTEM)/prelink-$(combo_os_arch).map
 endif
 
+ifeq ($(BOARD_USES_ECLAIR_LIBCAMERA),true)
+$(combo_target)GLOBAL_CFLAGS += -DUSE_ECLAIR_MEMORYDEALER
+endif
+
 # Now include the combo for this specific target.
 include $(BUILD_COMBOS)/$(combo_target)$(combo_os_arch).mk
 
