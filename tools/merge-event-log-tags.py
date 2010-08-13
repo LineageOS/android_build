8f99e7892d6e8dea78ae254215573663eafafef1
Eddie Ringle <eddie.ringle@gmail.com>
2010-08-12 23:23:49 -0400

    Use hashlib module instead of the deprecated md5 module
    Signed-off-by: Eddie Ringle <eddie.ringle@gmail.com>

--- tools/merge-event-log-tags.py
+++ tools/merge-event-log-tags.py
@@ -26,7 +26,7 @@ and fails if they do.
 
 import cStringIO
 import getopt
-import md5
+import hashlib
 import struct
 import sys
 
@@ -133,7 +133,7 @@ if warnings:
 # versions of python.  Using md5 is overkill here but is the same from
 # platform to platform and speed shouldn't matter in practice.
 def hashname(str):
-  d = md5.md5(str).digest()[:4]
+  d = hashlib.md5(str).digest()[:4]
   return struct.unpack("!I", d)[0]
 
 # Assign a tag number to all the entries that say they want one
