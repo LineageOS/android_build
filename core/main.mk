7d5830d792f17b118161d40fe5234a24fcd335db
Koushik Dutta <koushd@gmail.com>
2010-08-20 12:19:10 -0700

    quick hack to disable checkjni

--- core/main.mk
+++ core/main.mk
@@ -214,7 +214,7 @@ ifneq (,$(user_variant))
   
 else # !user_variant
   # Turn on checkjni for non-user builds.
-  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
+  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=0
   # Set device insecure for non-user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   # Allow mock locations by default for non user builds
