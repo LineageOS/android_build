30e43f85f8b83dd4210b86363ab912dd640050a0
Chris Soyars <ctso@ctso.me>
2010-12-23 00:44:33 +0100

    Add otapackage support for backuptool
    Change-Id: I512554c579d444067cd3ccbb0e6946a5eb6bc964a
    
    Modular backuptool.sh.  Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts.
    
    * Copy backuptool.functions alongside backuptool.sh.
    * Delete both from /system/bin as they are not useful there.
    
    Patch Series
    ============
    http://review.cyanogenmod.com/#change,13265
    CyanogenMod/android_build
      * edify generator
    http://review.cyanogenmod.com/#change,13266
    CyanogenMod/android_system_core
      * permissions on /system/addon.d
    http://review.cyanogenmod.com/#change,13267
    CyanogenMod/android_vendor_cm
      * 50-cm.sh reference backup script
      * modular backuptool.sh
      * support backuptool.functions used by /system/addon.d/*.sh scripts
    
    Change-Id: I26b4907d28f49c69627699d2accd2f0fa2d1b112
    
    update ota_from_target_files to handle mounting/unmounting for backupscript
    
    backupscript should not be mounting/unmounting itself as it makes other
    scripts have unexpected results (such as modelid_cfg, which expects /system
    to be mounted)
    
    instead have the ota script handle the mounting/unmounting
    
    Change-Id: I94511f4147c624d975cb3ecbeaa8b0e98f63437c
    
    build: Don't run backuptool on GMS builds
    
    Change-Id: I5dde27f9d16b88049171db9805221d92e67f3e5d

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -146,6 +146,16 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self.WordWrap(cmd))
 
+  def RunBackup(self, command):
+    self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
+    self.script.append('package_extract_file("system/bin/backuptool.functions", "/tmp/backuptool.functions");')
+    self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
+    self.script.append('set_perm(0, 0, 0644, "/tmp/backuptool.functions");')
+    self.script.append(('run_program("/tmp/backuptool.sh", "%s");' % command))
+    if command == "restore":
+        self.script.append('delete("/system/bin/backuptool.sh");')
+        self.script.append('delete("/system/bin/backuptool.functions");')
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
@@ -216,6 +226,12 @@ class EdifyGenerator(object):
           p.fs_type, common.PARTITION_TYPES[p.fs_type], p.device,
           p.mount_point, mount_flags))
       self.mounts.add(p.mount_point)
+
+  def Unmount(self, mount_point):
+    """Unmount the partition with the given mount_point."""
+    if mount_point in self.mounts:
+      self.mounts.remove(mount_point)
+      self.script.append('unmount("%s");' % (mount_point,))
 
   def UnpackPackageDir(self, src, dst):
     """Unpack a given directory from the OTA package into the given
