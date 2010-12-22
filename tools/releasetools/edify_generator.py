a1a273440a1b90664971b5a88f05bc73eef97de3
Chris Soyars <ctso@ctso.me>
2010-12-23 00:44:33 +0100

    releasetools: squash backuptool support
    this is a squash of the following commits:
    
    commit 30e43f85f8b83dd4210b86363ab912dd640050a0
    Author: Chris Soyars <ctso@ctso.me>
    Date:   Thu Dec 23 00:44:33 2010 +0100
    
        Add otapackage support for backuptool
    
        Change-Id: I512554c579d444067cd3ccbb0e6946a5eb6bc964a
    
        Modular backuptool.sh.  Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts.
    
        * Copy backuptool.functions alongside backuptool.sh.
        * Delete both from /system/bin as they are not useful there.
    
        Patch Series
        ============
        http://review.cyanogenmod.com/#change,13265
        CyanogenMod/android_build
          * edify generator
        http://review.cyanogenmod.com/#change,13266
        CyanogenMod/android_system_core
          * permissions on /system/addon.d
        http://review.cyanogenmod.com/#change,13267
        CyanogenMod/android_vendor_cm
          * 50-cm.sh reference backup script
          * modular backuptool.sh
          * support backuptool.functions used by /system/addon.d/*.sh scripts
    
        Change-Id: I26b4907d28f49c69627699d2accd2f0fa2d1b112
    
        update ota_from_target_files to handle mounting/unmounting for backupscript
    
        backupscript should not be mounting/unmounting itself as it makes other
        scripts have unexpected results (such as modelid_cfg, which expects /system
        to be mounted)
    
        instead have the ota script handle the mounting/unmounting
    
        Change-Id: I94511f4147c624d975cb3ecbeaa8b0e98f63437c
    
        build: Don't run backuptool on GMS builds
    
        Change-Id: I5dde27f9d16b88049171db9805221d92e67f3e5d
    
    commit 66ce73d1ffa81b9dd59639241f2e7172e5485a41
    Author: Tom Marshall <tdm@cyngn.com>
    Date:   Wed Apr 9 16:14:07 2014 -0700
    
        build: edify: use set_metadata for backuptool
    
        Also use saner permissions on backuptool.sh
    
        Change-Id: I50742b51867aa358f5924b8dc208833092a35bd9
    
    commit dac6e8fee851433ff35723f2a790e3930aaeb113
    Author: Ricardo Cerqueira <ricardo@cyngn.com>
    Date:   Wed Nov 5 18:16:43 2014 +0000
    
        ota: Include copies of the recovery scripts even if shipping in block mode
    
    commit 5a6d5b87d44f638307531f29c89d19b41c79532b
    Author: Ricardo Cerqueira <ricardo@cyngn.com>
    Date:   Wed Nov 5 19:58:59 2014 +0000
    
        ota: Fix recovery-script cleanups in block mode
    
        Change-Id: Iadb480f8062cfb7a0e6c9024e32ac0d71a7481d7
    
    Change-Id: I2401d334d8e16d18d3770b339ec2c787dd8e477c

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -156,6 +156,9 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self.WordWrap(cmd))
 
+  def RunBackup(self, command):
+    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
