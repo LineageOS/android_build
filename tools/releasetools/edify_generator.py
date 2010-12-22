d240037ad2104c88c6ea5e9a82a6c02832bf43c7
Chris Soyars <ctso@ctso.me>
2010-12-23 00:44:33 +0100

    Add otapackage support for backuptool
    Change-Id: I512554c579d444067cd3ccbb0e6946a5eb6bc964a

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -130,6 +130,11 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self._WordWrap(cmd))
 
+  def RunBackup(self, command):
+    self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
+    self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
+    self.script.append(('run_program("/tmp/backuptool.sh", "%s");' % command))
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
