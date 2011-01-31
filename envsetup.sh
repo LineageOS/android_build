68f05379291ed64628ba8a9ee2aa5a6c3d8f81a4
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-01-31 00:49:49 +0000

    Exclude default vendors from default CM lunch menu
    Ignore the vendorsetup.sh files from the devices themselves,
    since we're only interested in the CM product_* files for
    CyanogenMod builds. If the builder wants to include the
    stock vendor configs, they can be added to the menu by invoking
    envsetup like this:
    
    USE_DEFAULT_VENDORS=true . build/envsetup.sh
    
    Change-Id: Ia9e7ec8e1f429e2a9811f13cc126a66a04428f34

--- envsetup.sh
+++ envsetup.sh
@@ -1191,7 +1191,15 @@ fi
 unset _xarray
 
 # Execute the contents of any vendorsetup.sh files we can find.
-for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/build/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
+if [ "z${USE_DEFAULT_VENDORS}" != "z" ]; then
+    for f in `/bin/ls device/*/*/vendorsetup.sh 2>/dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+fi
+
+for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/build/vendorsetup.sh 2> /dev/null`
 do
     echo "including $f"
     . $f
