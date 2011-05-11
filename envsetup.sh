cf12070e7dec4b866213e8986db5504f984f1fb0
Jean-Baptiste Queru <jbq@google.com>
2011-05-11 10:40:43 -0700

    Remove simulator from the lunch menu
    Change-Id: I0d73b4722ce5126b64538ca7add2e55cd561c022

--- envsetup.sh
+++ envsetup.sh
@@ -435,12 +435,6 @@ function add_lunch_combo()
 # add the default one here
 add_lunch_combo full-eng
 
-# if we're on linux, add the simulator.  There is a special case
-# in lunch to deal with the simulator
-if [ "$(uname)" = "Linux" ] ; then
-    add_lunch_combo simulator
-fi
-
 function print_lunch_menu()
 {
     local uname=$(uname)
