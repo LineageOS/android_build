70b2227ea843932e8acbb44956be0dc9fc0d32f7
Jim Huang <jserv@0xlab.org>
2011-09-16 15:52:41 +0800

    Enable ICF link-time optimization and remove global inline options
    Removing global inline options gives compiler the opportunity
    to do inline optimizations and inline tunings for Android native
    codes.
    
    Benefit:
      - the size of almost all native libraries are reduced.
    
    The reference code size experiment:
    $ du -sb out/target/product/crespo/system/lib/
    [before]
        20843959	lib/
    [after]
        20726967	lib/
    
    code size reduction: 20843959 - 20726967 = 116992 bytes
    
    Backported from ICS
    
    Change-Id: I36bf036d1571a58d2345f30c72e9667fcbba768c

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -60,8 +60,7 @@ TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 TARGET_arm_CFLAGS :=    -O2 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
-                        -funswitch-loops     \
-                        -finline-limit=300
+                        -funswitch-loops
 
 # Modules can choose to compile some source as thumb. As
 # non-thumb enabled targets are supported, this is treated
@@ -71,8 +70,7 @@ ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
 TARGET_thumb_CFLAGS :=  -mthumb \
                         -Os \
                         -fomit-frame-pointer \
-                        -fno-strict-aliasing \
-                        -finline-limit=64
+                        -fno-strict-aliasing
 else
 TARGET_thumb_CFLAGS := $(TARGET_arm_CFLAGS)
 endif
@@ -97,6 +95,7 @@ arch_include_dir := $(dir $(android_config_h))
 TARGET_GLOBAL_CFLAGS += \
 			-msoft-float -fpic \
 			-ffunction-sections \
+			-fdata-sections \
 			-funwind-tables \
 			-fstack-protector \
 			-Wa,--noexecstack \
@@ -118,6 +117,7 @@ TARGET_GLOBAL_CFLAGS += -Wno-psabi
 
 TARGET_GLOBAL_LDFLAGS += \
 			-Wl,-z,noexecstack \
+			-Wl,--icf=safe \
 			$(arch_variant_ldflags)
 
 # We only need thumb interworking in cases where thumb support
@@ -136,8 +136,6 @@ TARGET_RELEASE_CFLAGS := \
 			-DNDEBUG \
 			-g \
 			-Wstrict-aliasing=2 \
-			-finline-functions \
-			-fno-inline-functions-called-once \
 			-fgcse-after-reload \
 			-frerun-cse-after-loop \
 			-frename-registers
