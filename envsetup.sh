b8c587c4568dc36f5557359c8bf37610845fb36a
Koushik Dutta <koushd@gmail.com>
2011-11-26 18:51:42 -0800

    roomservice delivers you lunch combos from the CyanogenMod github.
    fix roomservice formatting
    
    support product names with _ in them
    
    fix roomservice to handle pagination
    
    Change-Id: I4923c2f768094dbad8e06a72d9f27d46414030ab

--- envsetup.sh
+++ envsetup.sh
@@ -540,6 +540,15 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
+        # if we can't find a product, try to grab it off the CM github
+        T=$(gettop)
+        pushd $T > /dev/null
+        build/tools/roomservice.py $product
+        popd > /dev/null
+        check_product $product
+    fi
+    if [ $? -ne 0 ]
+    then
         echo
         echo "** Don't have a product spec for: '$product'"
         echo "** Do you have the right repo manifest?"
