629f5bcdee50ae00bd57750a03228c3f059947bd
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-28 09:05:16 +0700

    Disable dataroaming by default
    The property is removed so that the property in vendor/lineage
    can take effect.
    
    Change-Id: Iefadc1a6e71cbf92ecb7363c953f4b5a82064446

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -21,7 +21,11 @@
 
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
+
+ifeq ($(LINEAGE_BUILD),)
+PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dataroaming=true
+endif
 
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
