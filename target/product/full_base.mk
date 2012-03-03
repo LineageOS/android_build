d587c69344a01e4f4f9e06b24e78eef36af38da5
Arne Coucheron <arco68@gmail.com>
2012-03-03 22:53:06 +0100

    Fix inheritance of PRODUCT_PROPERTY_OVERRIDES for CM audio files
    Otherwise the ones set in CM vendor will be overridden by these.
    
    Change-Id: I3f87dfd009d8ddfd48972c15770599742b12c4af
    
    Author: Abhisek Devkota <ciwrl@cyanogenmod.com>
    Remove extra product property overrides
    
    This appears to clash with vendor/cm/common mk's for overrides; causes
    a broken default alarm (and OnTheHunt is so bleh).
    
    Change-Id: I5ec990d3c5ad56b573c45f1a3b20169c989851a0

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -27,11 +27,6 @@ PRODUCT_PACKAGES := \
     libwnndict \
     WAPPushManager
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
