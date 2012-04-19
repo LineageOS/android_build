d0357f7121ed73804e274e5c6cea77b00a28f84a
Ricardo Cerqueira <github@cerqueira.org>
2012-04-19 14:12:32 +0100

    roomservice: Fill in dependencies for already-deployed repositories
    Change-Id: Ia4c108025f75e955880b5c58df47d8888e9333b9

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -3,12 +3,20 @@ import os
 import sys
 import urllib2
 import json
+import re
 import netrc, base64
 from xml.etree import ElementTree
 
 product = sys.argv[1];
+
+if len(sys.argv) > 2:
+    depsonly = sys.argv[2]
+else:
+    depsonly = None
+
 device = product[product.index("_") + 1:]
-print "Device %s not found. Attempting to retrieve device repository from CyanogenMod Github (http://github.com/CyanogenMod)." % device
+if not depsonly:
+    print "Device %s not found. Attempting to retrieve device repository from CyanogenMod Github (http://github.com/CyanogenMod)." % device
 
 repositories = []
 
@@ -23,7 +31,7 @@ except:
     githubauth = None
 
 page = 1
-while True:
+while not depsonly:
     githubreq = urllib2.Request("https://api.github.com/users/CyanogenMod/repos?per_page=100&page=%d" % page)
     if githubauth:
         githubreq.add_header("Authorization","Basic %s" % githubauth)
@@ -56,6 +64,32 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
+def get_from_manifest(devicename):
+    try:
+        lm = ElementTree.parse(".repo/local_manifest.xml")
+        lm = lm.getroot()
+    except:
+        lm = ElementTree.Element("manifest")
+
+    for localpath in lm.findall("project"):
+        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+            return localpath.get("path")
+
+    return None
+
+def is_in_manifest(projectname):
+    try:
+        lm = ElementTree.parse(".repo/local_manifest.xml")
+        lm = lm.getroot()
+    except:
+        lm = ElementTree.Element("manifest")
+
+    for localpath in lm.findall("project"):
+        if localpath.get("name") == projectname:
+            return 1
+
+    return None
+
 def add_to_manifest(repositories):
     try:
         lm = ElementTree.parse(".repo/local_manifest.xml")
@@ -87,7 +121,7 @@ def add_to_manifest(repositories):
     f.write(raw_xml)
     f.close()
 
-def fetch_dependencies(repo_path, repositories):
+def fetch_dependencies(repo_path):
     print 'Looking for dependencies'
     dependencies_path = repo_path + '/cm.dependencies'
     syncable_repos = []
@@ -96,16 +130,17 @@ def fetch_dependencies(repo_path, repositories):
         dependencies_file = open(dependencies_path, 'r')
         dependencies = json.loads(dependencies_file.read())
         fetch_list = []
-        print dependencies
 
         for dependency in dependencies:
-            fetch_list.append(dependency)
-            syncable_repos.append(dependency['target_path'])
+            if not is_in_manifest("CyanogenMod/%s" % dependency['repository']):
+                fetch_list.append(dependency)
+                syncable_repos.append(dependency['target_path'])
 
         dependencies_file.close()
 
-        print 'Adding dependencies to manifest'
-        add_to_manifest(fetch_list)
+        if len(fetch_list) > 0:
+            print 'Adding dependencies to manifest'
+            add_to_manifest(fetch_list)
     else:
         print 'Dependencies file not found, bailing out.'
 
@@ -113,22 +148,32 @@ def fetch_dependencies(repo_path, repositories):
         print 'Syncing dependencies'
         os.system('repo sync %s' % ' '.join(syncable_repos))
 
-for repository in repositories:
-    repo_name = repository['name']
-    if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
-        print "Found repository: %s" % repository['name']
-        manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+if depsonly:
+    repo_path = get_from_manifest(device)
+    if repo_path:
+        fetch_dependencies(repo_path)
+    else:
+        print "Trying dependencies-only mode on a non-existing device tree?"
 
-        repo_path = "device/%s/%s" % (manufacturer, device)
+    sys.exit()
 
-        add_to_manifest([{'repository':repo_name,'target_path':repo_path}])
+else:
+    for repository in repositories:
+        repo_name = repository['name']
+        if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
+            print "Found repository: %s" % repository['name']
+            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
 
-        print "Syncing repository to retrieve project."
-        os.system('repo sync %s' % repo_path)
-        print "Repository synced!"
+            repo_path = "device/%s/%s" % (manufacturer, device)
 
-        fetch_dependencies(repo_path, repositories)
-        print "Done"
-        sys.exit()
+            add_to_manifest([{'repository':repo_name,'target_path':repo_path}])
+
+            print "Syncing repository to retrieve project."
+            os.system('repo sync %s' % repo_path)
+            print "Repository synced!"
+
+            fetch_dependencies(repo_path)
+            print "Done"
+            sys.exit()
 
 print "Repository for %s not found in the CyanogenMod Github repository list. If this is in error, you may need to manually add it to your local_manifest.xml." % device
