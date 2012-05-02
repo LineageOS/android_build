40a40e357510f3b68e94706d14447c60a6b543df
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-05-02 23:53:25 +0800

    Modified ARM v7 build environment to make it more developer friendly
    1. No longer prefers the neon FPU unit when TARGET_ARCH_VARIANT_FPU is not set
    2. Enabled mfpu, mcpu, and mtune code optimization
    3. The new mk files should be better optimized for devices where the developer hasn't set TARGET_ARCH_VARIANT_FPU
    
    Based on http://review.cyanogenmod.com/#/c/15478/.
    
    Change-Id: I44be3712e789f1f6fd40c58b391adcc18efb4ce8

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -8,38 +8,61 @@ ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
 ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
-ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),none)
-ARCH_ARM_HAVE_VFP               := true
-else
-ARCH_ARM_HAVE_VFP               := false
-endif
 
 ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
-TARGET_ARCH_VARIANT_FPU         := neon
+ARCH_ARM_HAVE_VFP               := false
+else
+ARCH_ARM_HAVE_VFP               := true
 endif
 
 ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),neon)
+ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
-endif
-
-ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
-TARGET_ARCH_VARIANT_CPU         := cortex-a8
 endif
 
 ifeq ($(strip $(TARGET_CPU_SMP)),true)
 ARCH_ARM_HAVE_TLS_REGISTER      := true
 endif
 
+# We don't really need the following tests for checking
+# whether the compiler supports a given mcpu argument since
+# we include our own cross compiler, but it makes it easier to
+# maintain when we change the compiler.
+
+mcpu-arg = $(shell sed 's/^-mcpu=//' <<< $(call cc-option,-mcpu=$(1),-mcpu=$(2)))
+
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a15)
+	TARGET_ARCH_VARIANT_CPU := $(call mcpu-arg,cortex-a15,cortex-a9)
+endif
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a9)
+	TARGET_ARCH_VARIANT_CPU := $(call mcpu-arg,cortex-a9,cortex-a8)
+endif
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a8)
+	TARGET_ARCH_VARIANT_CPU := $(call mcpu-arg,cortex-a8,)
+endif
+
+
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
 arch_variant_cflags := \
-    -march=armv7-a \
-    -mtune=$(TARGET_ARCH_VARIANT_CPU)
+    -march=armv7-a
+else
+arch_variant_cflags := \
+    -mcpu=$(strip $(TARGET_ARCH_VARIANT_CPU)) \
+    -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
+endif
 
 ifeq ($(strip $(ARCH_ARM_HAVE_VFP)),true)
-arch_variant_cflags += -mfloat-abi=softfp
-arch_variant_cflags += -mfpu=$(TARGET_ARCH_VARIANT_FPU)
+arch_variant_cflags += \
+	-mfloat-abi=softfp \
+	-mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
 else
-arch_variant_cflags += -mfloat-abi=soft
+arch_variant_cflags += \
+	-mfloat-abi=soft
 endif
+
+arch_variant_cflags += \
+    -D__ARM_ARCH_7__ \
+    -D__ARM_ARCH_7A__
 
 ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a8)
 arch_variant_ldflags := \
