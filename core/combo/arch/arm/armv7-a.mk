dd169f149f993e60d29b32bbe698cef889cbd71b
UnknownzD <UnknownzD@gmail.com>
2012-05-02 23:53:25 +0800

    ARM v7 build environment to make it more user friendly, and also without those default setting which prefered specific devices, which could possibly makes developer unables to properly build all the stuffs
    Change-Id: I44be3712e789f1f6fd40c58b391adcc18efb4ce8

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -8,22 +8,15 @@ ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
 ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
-ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),none)
-ARCH_ARM_HAVE_VFP               := true
-else
-ARCH_ARM_HAVE_VFP               := false
-endif
 
 ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
-TARGET_ARCH_VARIANT_FPU         := neon
+ARCH_ARM_HAVE_VFP               := false
+else
+ARCH_ARM_HAVE_VFP               := true
 endif
 
 ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),neon)
 ARCH_ARM_HAVE_NEON              := true
-endif
-
-ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
-TARGET_ARCH_VARIANT_CPU         := cortex-a8
 endif
 
 ifeq ($(strip $(TARGET_CPU_SMP)),true)
@@ -32,11 +25,18 @@ endif
 
 arch_variant_cflags := \
     -march=armv7-a \
-    -mtune=$(TARGET_ARCH_VARIANT_CPU)
+    -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU)) \
+    -mcpu=$(strip $(TARGET_ARCH_VARIANT_CPU)) \
+    -D__ARM_ARCH_7__ \
+    -D__ARM_ARCH_7A__
 
 ifeq ($(strip $(ARCH_ARM_HAVE_VFP)),true)
-arch_variant_cflags += -mfloat-abi=softfp
-arch_variant_cflags += -mfpu=$(TARGET_ARCH_VARIANT_FPU)
+ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
+arch_variant_cflags += -D__ARM_NEON__ -D__ARM_HAVE_NEON
+else
+arch_variant_cflags += -D__VFP_FP__ -D__ARM_HAVE_VFP
+endif
+arch_variant_cflags += -mfloat-abi=softfp -mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
 else
 arch_variant_cflags += -mfloat-abi=soft
 endif
