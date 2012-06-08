b2cd6e17d2b7fbe183928e700a91289558f0513a
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-06-08 01:32:40 +0100

    eat: Adapt to new file naming scheme
    Change-Id: I101e333918bc215bebcc594c97bdd68307c90aaa

--- envsetup.sh
+++ envsetup.sh
@@ -613,7 +613,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=`sed -n -e'/ro\.cm\.version/s/.*=//p' $OUT/system/build.prop`
-        ZIPFILE=update-cm-$MODVERSION-signed.zip
+        ZIPFILE=cm-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
