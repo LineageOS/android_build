e0c1f1bf296dd54681a65131ae82491f5b367668
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-06-10 12:36:09 +0200

    use linaro 4.7.1 toolchain
    Change-Id: I61348be6e8f3390b8c491b3ee3e29b08ce4a2cd1

--- envsetup.sh
+++ envsetup.sh
@@ -134,13 +134,13 @@ function setpaths()
 
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
     export ANDROID_EABI_TOOLCHAIN=
-    toolchaindir=toolchain/arm-linux-androideabi-4.4.x/bin
+    toolchaindir=toolchain/linaro-4.7.1/bin
     if [ -d "$prebuiltdir/$toolchaindir" ]; then
         export ANDROID_EABI_TOOLCHAIN=$prebuiltdir/$toolchaindir
     fi
 
     export ARM_EABI_TOOLCHAIN=
-    toolchaindir=toolchain/arm-eabi-4.4.3/bin
+    toolchaindir=toolchain/linaro-4.7.1/bin
     if [ -d "$prebuiltdir/$toolchaindir" ]; then
         export ARM_EABI_TOOLCHAIN=$prebuiltdir/$toolchaindir
     fi
