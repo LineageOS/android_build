5cc45b12d823144e3045e498a837f0b356d6111a
Andreas Schneider <asn@cryptomilk.org>
2012-08-31 22:45:52 +0200

    build: Remove wallpapers from full_base.
    They are already in the cm common config for the full phone.
    
    Change-Id: I02fc0ea73e92ae8b19931d05e6783e3cfa166af2

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -27,16 +27,6 @@ PRODUCT_PACKAGES := \
     libwnndict \
     WAPPushManager
 
-PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    PhotoTable
-
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
