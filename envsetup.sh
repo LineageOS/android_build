bbe5ec0b43876db96b5a7a36ece25ed39c1bf458
Alan Orth <alan.orth@gmail.com>
2012-09-07 11:44:27 +0300

    envsetup.sh: Fix the `reposync` function
    It seems ionice can't find `repo` if it's in ~/bin, even if ~/bin
    is in the user's $PATH.  Placing repo into /usr/bin or /usr/local/
    bin works, but is undesirable (build instructions from both AOSP
    and CM tell users to put repo into ~/bin, and repo likes to update
    itself from time to time).
    
    This forces the reposync function to use the full path to the repo
    binary.
    
    Change-Id: I9dc4a3d4ec3f39563e3a84de7321189700079c2e

--- envsetup.sh
+++ envsetup.sh
@@ -1919,7 +1919,7 @@ function reposync() {
             repo sync -j 4 "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 repo sync -j 4 "$@"
+            schedtool -B -n 1 -e ionice -n 1 `which repo` sync -j 4 "$@"
             ;;
     esac
 }
