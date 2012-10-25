ae0a99782d900fa539e7521032a87b8bb382e319
Ameya Thakur <ameyat@codeaurora.org>
2012-10-24 19:31:42 -0700

    Fix case where boot/recovery.img were being built with wrong params.
    The boot and recovery images  now get built using the same params during ota package
    generation as during a normal build.
    
    Change-Id: I93d46e11a4245288f0e87c87a2e4bf45ac5aff69

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -328,6 +328,21 @@ def BuildBootableImage(sourcedir, fs_config_file, info_dict=None):
     cmd.append("--base")
     cmd.append(open(fn).read().rstrip("\n"))
 
+  fn = os.path.join(sourcedir, "tagsaddr")
+  if os.access(fn, os.F_OK):
+    cmd.append("--tags-addr")
+    cmd.append(open(fn).read().rstrip("\n"))
+
+  fn = os.path.join(sourcedir, "ramdisk_offset")
+  if os.access(fn, os.F_OK):
+    cmd.append("--ramdisk_offset")
+    cmd.append(open(fn).read().rstrip("\n"))
+
+  fn = os.path.join(sourcedir, "dt_args")
+  if os.access(fn, os.F_OK):
+    cmd.append("--dt")
+    cmd.append(open(fn).read().rstrip("\n"))
+
   fn = os.path.join(sourcedir, "pagesize")
   if os.access(fn, os.F_OK):
     cmd.append("--pagesize")
@@ -339,7 +354,6 @@ def BuildBootableImage(sourcedir, fs_config_file, info_dict=None):
 
   cmd.extend(["--ramdisk", ramdisk_img.name,
               "--output", img.name])
-
   p = Run(cmd, stdout=subprocess.PIPE)
   p.communicate()
   assert p.returncode == 0, "mkbootimg of %s image failed" % (
