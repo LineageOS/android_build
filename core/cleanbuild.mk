be3a366740eac464e41b3f7d7685b19fe89a36ee
Chirayu Desai <chirayudesai1@gmail.com>
2012-11-01 12:58:45 +0530

    cleanbuild: add *.zip.md5sum to installclean_files
    Change-Id: I4b68bb9a8d421a54b008c5318fa82ed230bd6441

--- core/cleanbuild.mk
+++ core/cleanbuild.mk
@@ -234,6 +234,7 @@ installclean_files := \
 	$(PRODUCT_OUT)/*.xlb \
 	$(PRODUCT_OUT)/*.zip \
 	$(PRODUCT_OUT)/kernel \
+	$(PRODUCT_OUT)/*.zip.md5sum \
 	$(PRODUCT_OUT)/data \
 	$(PRODUCT_OUT)/skin \
 	$(PRODUCT_OUT)/obj/APPS \
