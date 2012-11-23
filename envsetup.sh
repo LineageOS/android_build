f63b1273be8c01ebb636b009e51433d9d7830e6d
Arnav Gupta <championswimmer@gmail.com>
2012-11-23 10:47:44 -0700

    mka: change how make is called
    in some environments make -j N xxx results in compiling target 'N' instead of 'xxx'
    make -jN xxx will always make 'xxx' with N number of threads
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>
    
    Change-Id: I77f17c9286a5ef1d61163d8f978900fb4c24e2e7

--- envsetup.sh
+++ envsetup.sh
@@ -2046,7 +2046,7 @@ function mka() {
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
             ;;
     esac
 }
