0112612eac93f3d25c2f744639035c1d88a2e797
Koushik Dutta <koushd@gmail.com>
2012-12-24 02:07:34 -0800

    mkvendor.sh: fixes for Android 4.2.
    Change-Id: Id7ff9ef03700d595734811ccf868371ffb024384

--- tools/device/mkvendor.sh
+++ tools/device/mkvendor.sh
@@ -56,7 +56,7 @@ then
     echo Output will be in $DEVICE_DIR
     mkdir -p $DEVICE_DIR
 
-    TMPDIR=/tmp/bootimg
+    TMPDIR=/tmp/$(whoami)/bootimg
     rm -rf $TMPDIR
     mkdir -p $TMPDIR
     cp $BOOTIMAGE $TMPDIR
@@ -114,6 +114,12 @@ fi
 
 mv $DEVICE_DIR/device.mk $DEVICE_DIR/device_$DEVICE.mk
 
+echo Creating initial git repository.
+pushd $DEVICE_DIR
+git init
+git add .
+git commit -a -m "mkvendor.sh: Initial commit of $DEVICE"
+popd
 
 echo Done!
 echo Use the following command to set up your build environment:
