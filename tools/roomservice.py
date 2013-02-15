f860707c4d962e0f8e6aecdf24caa4e132696a2b
Pedro Ângelo <pangelo@void.io>
2013-02-15 23:41:38 +0000

    Fix fallback branch search in roomservice
    If you provided a fallback branch to roomservice via the
    ROOMSERVICE_BRANCHES environment var the branch search would fail
    if the device repo had any tags.
    
    Fixed this by appending the tag search results to the branch
    search results instead of overwriting them
    
    Change-Id: I73a11af1500bd04e346f08ec3f83454502f3a169

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -225,7 +225,7 @@ else:
             if not has_branch(result, default_revision):
                 githubreq = urllib2.Request(repository['tags_url'].replace('{/tag}', ''))
                 add_auth(githubreq)
-                result = json.loads(urllib2.urlopen(githubreq).read())
+                result.extend (json.loads(urllib2.urlopen(githubreq).read()))
             
             repo_path = "device/%s/%s" % (manufacturer, device)
             adding = {'repository':repo_name,'target_path':repo_path}
