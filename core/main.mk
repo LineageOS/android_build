dcbc0a6831a8ef404de5dba57e77559ac7ecfcf8
Danny Baumann <dannybaumann@web.de>
2013-02-15 13:12:27 +0100

    Enable ADB authentication by default.
    Change-Id: Ie6fd753cbf4eb2477a1d5077d67627dcc61d493d

--- core/main.mk
+++ core/main.mk
@@ -288,7 +288,7 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1 ro.adb.secure=1
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
