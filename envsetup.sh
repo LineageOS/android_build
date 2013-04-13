97d84a2dc882f8c97fcc20bdcd5457792828862d
Steve Kondik <shade@chemlab.org>
2013-04-13 13:23:35 -0700

    envsetup: Make installboot work in recovery mode too
     * Needs a change to adb to allow waiting for recovery
    
    Change-Id: Ia90645513f46bcb5f20b7c74c55ef15842710d3d

--- envsetup.sh
+++ envsetup.sh
@@ -1702,9 +1702,8 @@ function installboot()
     adb start-server
     adb root
     sleep 1
-    adb wait-for-device
-    adb remount
-    adb wait-for-device
+    adb wait-for-online shell mount /system 2>&1 > /dev/null
+    adb wait-for-online remount
     if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
     then
         adb push $OUT/boot.img /cache/
@@ -1741,9 +1740,8 @@ function installrecovery()
     adb start-server
     adb root
     sleep 1
-    adb wait-for-device
-    adb remount
-    adb wait-for-device
+    adb wait-for-online shell mount /system 2>&1 >> /dev/null
+    adb wait-for-online remount
     if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
     then
         adb push $OUT/recovery.img /cache/
