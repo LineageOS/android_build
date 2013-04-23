f10f507692b047e5302d776694fcc7026ee6b72f
Matt Mower <mowerm@gmail.com>
2013-04-23 12:09:53 -0500

    mkvendor: fix echoed build commands
    cm.mk.template uses PRODUCT_NAME := cm___DEVICE__
    
    Change-Id: I063fc4b93d90ebb034637bf3e92d83c77aa439ca

--- tools/device/mkvendor.sh
+++ tools/device/mkvendor.sh
@@ -123,6 +123,6 @@ popd
 
 echo Done!
 echo Use the following command to set up your build environment:
-echo '  'lunch full_$DEVICE-eng
+echo '  'lunch cm_$DEVICE-eng
 echo And use the follwowing command to build a recovery:
-echo '  '. build/tools/device/makerecoveries.sh full_$DEVICE-eng
+echo '  '. build/tools/device/makerecoveries.sh cm_$DEVICE-eng
