9bcbcd349edf095abca1a7a0428a1e1e19e9cc60
Chirayu Desai <cdesai@cyanogenmod.org>
2013-05-04 17:59:18 +0530

    eat: get the proper MODVERSION
    * The version in system/build.prop might be
      different than what the zip is named on incremental
      builds.
    * Use CM_VERSION from the build system instead.
    
    Change-Id: If4508921d8fd05219a2f358a2397a13b247065bd
    Signed-off-by: Chirayu Desai <cdesai@cyanogenmod.org>

--- envsetup.sh
+++ envsetup.sh
@@ -727,7 +727,7 @@ function tapas()
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=`sed -n -e'/ro\.cm\.version/s/.*=//p' $OUT/system/build.prop`
+        MODVERSION=$(get_build_var CM_VERSION)
         ZIPFILE=cm-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
