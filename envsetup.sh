78d0ac87cc19ed98a5737ecc94286341b1a3c5fe
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-05 21:45:51 +0530

    envsetup: hmm repopick
    Change-Id: I483cb36721f31dbf1f67e46cbe8a306b2a9e2c15

--- envsetup.sh
+++ envsetup.sh
@@ -22,6 +22,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - mkap:     Builds the module(s) using mka and pushes them to the device.
 - cmka:     Cleans and builds using mka.
 - reposync: Parallel repo sync using ionice and SCHED_BATCH.
+- repopick: Utility to fetch changes from Gerrit.
 - installboot: Installs a boot.img to the connected device.
 - installrecovery: Installs a recovery.img to the connected device.
 
