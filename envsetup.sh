e9e4d8f70d83de22627d0453dba0f37711e58d98
Dave Carey <carvsdriver@gmail.com>
2013-06-27 10:17:28 -0700

    build: update toolchain to linaro 4.7.3
    Change-Id: Ie804a1452dff1f5e13ceb8f2d9a050041ad2d643

--- envsetup.sh
+++ envsetup.sh
@@ -144,7 +144,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-4.6/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-4.6/bin
+        arm) toolchaindir=arm/arm-eabi-linaro-4.7.3/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-4.6/bin
             ;;
