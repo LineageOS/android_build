a0f10adc44a3ebed13b38d7cc1635bc3d6f75fd1
cybojenix <anthonydking@gmail.com>
2013-06-28 20:30:00 +0100

    lets make the lunch list look a little neater
    It's very simple, but with more and more devices coming in, a long list won't cut it
    There are probably better ways of doing this, feel free to comment
    
    Change-Id: I9bd1fef920f985a133c882df9987b6be29dbc7b8

--- envsetup.sh
+++ envsetup.sh
@@ -578,9 +578,9 @@ function print_lunch_menu()
     local choice
     for choice in ${LUNCH_MENU_CHOICES[@]}
     do
-        echo "     $i. $choice"
+        echo " $i. $choice "
         i=$(($i+1))
-    done
+    done | column
 
     if [ "z${CM_DEVICES_ONLY}" != "z" ]; then
        echo "... and don't forget the bacon!"
