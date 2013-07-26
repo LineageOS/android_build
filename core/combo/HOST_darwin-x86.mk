56e719ae105a33f21746a8d9932d3cbd63d4bb18
Shareef Ali <shareefalis@cyanogenmod.org>
2013-07-25 20:01:35 -0500

    build:  fix merge derp
    Change-Id: Ibe1b9da0a51ed593fe8d564ad33d5edae8c38ecd

--- core/combo/HOST_darwin-x86.mk
+++ core/combo/HOST_darwin-x86.mk
@@ -54,6 +54,7 @@ ifeq ($(mac_sdk_version),)
 mac_sdk_version := $(firstword $(mac_sdk_versions_supported))
 endif
 endif
+endif
 
 mac_sdk_path := $(shell xcode-select -print-path)
 # try /Applications/Xcode*.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.?.sdk
@@ -78,20 +79,18 @@ endif
 
 HOST_TOOLCHAIN_ROOT := prebuilts/gcc/darwin-x86/host/i686-apple-darwin-4.2.1
 HOST_TOOLCHAIN_PREFIX := $(HOST_TOOLCHAIN_ROOT)/bin/i686-apple-darwin$(gcc_darwin_version)
+
 # Don't do anything if the toolchain is not there
 ifneq (,$(strip $(wildcard $(HOST_TOOLCHAIN_PREFIX)-gcc)))
-HOST_CC  := $(HOST_TOOLCHAIN_PREFIX)-gcc
+HOST_CC := $(HOST_TOOLCHAIN_PREFIX)-gcc
 HOST_CXX := $(HOST_TOOLCHAIN_PREFIX)-g++
-
-ifeq ($(mac_sdk_version),10.9)
-HOST_GLOBAL_CFLAGS += -I$(mac_sdk_root)/usr/include/c++/4.2.1 -arch i386 -Wno-nested-anon-types -Wno-unused-parameter
-HOST_GLOBAL_LDFLAGS += -Wl,-arch,i386,-lstdc++
-endif
-else
 ifeq ($(mac_sdk_version),10.8)
 # Mac SDK 10.8 no longer has stdarg.h, etc
 host_toolchain_header := $(HOST_TOOLCHAIN_ROOT)/lib/gcc/i686-apple-darwin$(gcc_darwin_version)/4.2.1/include
 HOST_GLOBAL_CFLAGS += -isystem $(host_toolchain_header)
+elif ($(mac_sdk_version),10.9)
+HOST_GLOBAL_CFLAGS += -I$(mac_sdk_root)/usr/include/c++/4.2.1 -arch i386 -Wno-nested-anon-types -Wno-unused-parameter
+HOST_GLOBAL_LDFLAGS += -Wl,-arch,i386,-lstdc++
 endif
 else
 HOST_CC := gcc
