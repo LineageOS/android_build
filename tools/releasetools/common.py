3791e4db2e843c4b39a1e0b6ec314a5774e23868
Ameya Thakur <ameyat@codeaurora.org>
2013-07-29 17:39:37 -0700

    build: Add changes to release tools and mkbootimg
    Change the prototype of LoadRecoveryFstab to take in the device type
    as a argument. Fix the case where mkbootimg was being passed a incorrect
    argument.
    
    Change-Id: Ic6ac596d8d96d3a5effbdf513763ec1cb92b1a03

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -230,6 +230,9 @@ def LoadInfoDict(input_file, input_dir=None):
             vendor_base_fs_file,)
         del d["vendor_base_fs_file"]
 
+
+  if "device_type" not in d:
+    d["device_type"] = "MMC"
   try:
     data = read_helper("META/imagesizes.txt")
     for line in data.split("\n"):
@@ -263,7 +266,7 @@ def LoadInfoDict(input_file, input_dir=None):
     d["fstab"] = None
   else:
     d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"],
-                                   d.get("system_root_image", False))
+                                   d.get("system_root_image", d["device_type"], False))
   d["build.prop"] = LoadBuildProp(read_helper)
   return d
 
@@ -286,7 +289,7 @@ def LoadDictionaryFromLines(lines):
       d[name] = value
   return d
 
-def LoadRecoveryFSTab(read_helper, fstab_version, system_root_image=False):
+def LoadRecoveryFSTab(read_helper, fstab_version, type, system_root_image=False):
   class Partition(object):
     def __init__(self, mount_point, fs_type, device, length, device2, context):
       self.mount_point = mount_point
@@ -466,6 +469,11 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
     fn = os.path.join(sourcedir, "tagsaddr")
     if os.access(fn, os.F_OK):
       cmd.append("--tags-addr")
+      cmd.append(open(fn).read().rstrip("\n"))
+
+    fn = os.path.join(sourcedir, "tags_offset")
+    if os.access(fn, os.F_OK):
+      cmd.append("--tags_offset")
       cmd.append(open(fn).read().rstrip("\n"))
 
     fn = os.path.join(sourcedir, "ramdisk_offset")
