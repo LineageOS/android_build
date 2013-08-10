7ebf277da87f602a8c01bca472589c98f3af6d2f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-08-10 15:03:49 +0200

    kernel: add a new TARGET_KERNEL_TOOLCHAIN var to support aosp gcc 4.7
    Change-Id: I310ca2edf531e11f45ccc544256c608a462db901

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -141,7 +141,11 @@ ifeq ($(TARGET_ARCH),arm)
             ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
         endif
     else
-        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+        ifneq ($(TARGET_KERNEL_TOOLCHAIN),)
+            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/$(TARGET_KERNEL_TOOLCHAIN)"
+        else
+            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+        endif
     endif
     ccache = 
 endif
