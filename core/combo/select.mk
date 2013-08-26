03491a97013540e33a445941da16927ecff73b08
Mike Grissom <mikeyman77@gmail.com>
2013-08-26 16:09:51 -0700

    Don't set CCACHE_BASEDIR if its already set
     * enable use of shared ccache
    
    Change-Id: Ic708a5d5169291d674b167aa2c87bf2c8adaafae

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -80,7 +80,9 @@ ifneq ($(USE_CCACHE),)
   # We don't really use system headers much so the rootdir is
   # fine; ensures these paths are relative for all Android trees
   # on a workstation.
-  export CCACHE_BASEDIR := $(ANDROID_BUILD_TOP)
+  ifeq ($(CCACHE_BASEDIR),)
+    export CCACHE_BASEDIR := $(ANDROID_BUILD_TOP)
+  endif
 
   # Workaround for ccache with clang.
   # See http://petereisentraut.blogspot.com/2011/09/ccache-and-clang-part-2.html
