e04d4ddad4790a5f67d96873890cdc8230f0e18a
Koushik Dutta <koushd@gmail.com>
2013-09-24 20:55:01 -0700

    Fix the build for Xcode 5.
    Change-Id: Icf8a4917d23114dae62a0e8ac3563587c3bf0361

--- core/combo/HOST_darwin-x86.mk
+++ core/combo/HOST_darwin-x86.mk
@@ -87,10 +87,12 @@ HOST_CXX := $(HOST_TOOLCHAIN_PREFIX)-g++
 ifeq ($(mac_sdk_version),10.9)
 HOST_GLOBAL_CFLAGS += -I$(mac_sdk_root)/usr/include/c++/4.2.1 -arch i386 -Wno-nested-anon-types -Wno-unused-parameter
 HOST_GLOBAL_LDFLAGS += -Wl,-arch,i386,-lstdc++
-elif ($(mac_sdk_version),10.8)
+else
+ifeq ($(mac_sdk_version),10.8)
 # Mac SDK 10.8 no longer has stdarg.h, etc
 host_toolchain_header := $(HOST_TOOLCHAIN_ROOT)/lib/gcc/i686-apple-darwin$(gcc_darwin_version)/4.2.1/include
 HOST_GLOBAL_CFLAGS += -isystem $(host_toolchain_header)
+endif
 endif
 else
 HOST_CC := gcc
