2a2cdad57d8c585fda6e57c04b12139b5de4cd61
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-27 15:43:26 +0100

    Fix exporting the unique build identifier
    Change-Id: Ie58e981b7e83a4351d137dfd8995dce7d365d950

--- envsetup.sh
+++ envsetup.sh
@@ -74,7 +74,7 @@ function check_product()
 
     if (echo -n $1 | grep -q -e "^cm_") ; then
        CM_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
-       BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | sha1sum | cut -c1-10)
+       export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | sha1sum | cut -c1-10)
     else
        CM_BUILD=
     fi
