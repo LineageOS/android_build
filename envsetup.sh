efc218ad92aed1be36d00ba5ff0fefc74fe898ef
Steve Kondik <shade@chemlab.org>
2013-10-27 13:34:36 -0700

    build: Add cafremote command to envsetup
     * So lazy!
    
    Change-Id: I99a988180abb4fb486d4ebbb842bb9eda03bf1bc

--- envsetup.sh
+++ envsetup.sh
@@ -27,6 +27,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - cmgerrit: A Git wrapper that fetches/pushes patch from/to CM Gerrit Review
 - cmrebase: Rebase a Gerrit change and push it again
 - aospremote: Add git remote for matching AOSP repository
+- cafremote: Add git remote for matching CodeAurora repository.
 - mka:      Builds using SCHED_BATCH on all processors
 - reposync: Parallel repo sync using ionice and SCHED_BATCH
 - repopick: Utility to fetch changes from Gerrit.
@@ -1694,6 +1695,24 @@ function aospremote()
     echo "Remote 'aosp' created"
 }
 export -f aospremote
+
+function cafremote()
+{
+    git remote rm caf 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    PROJECT=`pwd | sed s#$ANDROID_BUILD_TOP/##g`
+    if (echo $PROJECT | grep -qv "^device")
+    then
+        PFX="platform/"
+    fi
+    git remote add caf git://codeaurora.org/$PFX$PROJECT
+    echo "Remote 'caf' created"
+}
+export -f cafremote
+
 
 function installboot()
 {
