df6ecbdaadd03cc5f8f656615b20c27343286a93
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-05 22:17:59 +0000

    cm: Fix global selinux policy inclusion
    We're adding CM-specific policies to an overlay instead of patching
    external/sepolicy, so we need to make sure it's included to prevent
    misbehaving systems
    
    Change-Id: I7989a1093f9abc8360d492e73a860eb4afad10ad

--- core/config.mk
+++ core/config.mk
@@ -617,4 +617,10 @@ RS_PREBUILT_CLCORE := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/librsrt_$(TA
 RS_PREBUILT_LIBPATH := -L prebuilts/ndk/8/platforms/android-9/arch-$(TARGET_ARCH)/usr/lib
 RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcompiler_rt.a
 
+ifneq ($(CM_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/cm/sepolicy/sepolicy.mk)
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
