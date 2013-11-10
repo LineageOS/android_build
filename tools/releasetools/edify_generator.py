d5dc3e14455e501eb188df7611ed7259dcb49145
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-11-10 12:20:37 +0700

    build: edify: fix multiple device assert
    Only generate one error message since multiple error message
    will trigger a fail if the device does not match the first
    assert
    
    Change-Id: I1dc47cce10032eb28bbb808482b5ee9efc3b87bc

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -93,11 +93,11 @@ class EdifyGenerator(object):
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
     cmd = ('assert(' +
-           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s" || '
-           'abort("This package is for \\"%s\\" devices; '
+           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+                         % (i, i) for i in device.split(",")]) +
+           ' || abort("This package is for \\"%s\\" devices; '
            'this is a \\"" + getprop("ro.product.device") + "\\".");'
-                         % (i, i, i) for i in device.split(",")]) +
-           ');')
+           ');') % device
     self.script.append(self._WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
