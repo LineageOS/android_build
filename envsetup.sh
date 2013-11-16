2dc96853332d2c5b7b696123574783292312cc63
Steve Kondik <shade@chemlab.org>
2013-11-16 03:48:30 -0800

    envsetup: Fix remote functions to deal with symlinks
    Change-Id: I6f35dc30dda22149b2c37e1c6c7d41ea7239d3bb

--- envsetup.sh
+++ envsetup.sh
@@ -1502,7 +1502,7 @@ function aospremote()
     then
         echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
     fi
-    PROJECT=`pwd | sed s#$ANDROID_BUILD_TOP/##g`
+    PROJECT=`pwd -P | sed s#$ANDROID_BUILD_TOP/##g`
     if (echo $PROJECT | grep -qv "^device")
     then
         PFX="platform/"
@@ -1519,7 +1519,7 @@ function cafremote()
     then
         echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
     fi
-    PROJECT=`pwd | sed s#$ANDROID_BUILD_TOP/##g`
+    PROJECT=`pwd -P | sed s#$ANDROID_BUILD_TOP/##g`
     if (echo $PROJECT | grep -qv "^device")
     then
         PFX="platform/"
