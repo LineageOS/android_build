ab46c15806af94b4eb8b738db91e933abfb088e3
Ethan Chen <intervigil@gmail.com>
2014-06-02 16:49:59 -0700

    Generate extra userdata partition if needed
    * To support variants of devices which may come in 16/32/64GB variants.
    
    Change-Id: I74c32d8316f0450a1445fe95a95e5cabb7a9dd1b

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -57,8 +57,12 @@ def BuildImage(in_dir, prop_dict, out_file):
     if "extfs_sparse_flag" in prop_dict:
       build_command.append(prop_dict["extfs_sparse_flag"])
       #run_fsck = True
-    build_command.extend([in_dir, out_file, fs_type,
-                          prop_dict["mount_point"]])
+    if "is_userdataextra" in prop_dict:
+      build_command.extend([in_dir, out_file, fs_type,
+                           "data"])
+    else:
+      build_command.extend([in_dir, out_file, fs_type,
+                            prop_dict["mount_point"]])
     if "partition_size" in prop_dict:
       build_command.append(prop_dict["partition_size"])
     if "selinux_fc" in prop_dict:
@@ -125,6 +129,11 @@ def ImagePropFromGlobalDict(glob_dict, mount_point):
   elif mount_point == "data":
     copy_prop("fs_type", "fs_type")
     copy_prop("userdata_size", "partition_size")
+  elif mount_point == "data_extra":
+    copy_prop("fs_type", "fs_type")
+    copy_prop("userdataextra_size", "partition_size")
+    copy_prop("userdataextra_name", "partition_name")
+    d["is_userdataextra"] = True
   elif mount_point == "cache":
     copy_prop("cache_fs_type", "fs_type")
     copy_prop("cache_size", "partition_size")
