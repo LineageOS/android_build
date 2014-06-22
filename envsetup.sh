7cf5126d1a1febe7a573824e1eccda35a54e9d7e
JustArchi <JustArchi@JustArchi.net>
2014-06-22 14:37:30 +0200

    Build: Add support for specifying build variant in brunch/breakfast
    This little modification allows specifying build variant in brunch/
    breakfast commands. For example we can use "brunch i9300 user" to build
    user variant instead of default userdebug.
    
    When no extra argument is given, userdebug is default.
    
    Change-Id: I935327252098ee74b34a815a023d2ac6b9a53a30

--- envsetup.sh
+++ envsetup.sh
@@ -606,6 +606,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     CM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
@@ -626,7 +627,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the CM model name
-            lunch cm_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch cm_$target-$variant
         fi
     fi
     return $?
