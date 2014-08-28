ede1422aa825d85e90d9a39cf88684ba202b2b0c
Matt Mower <mowerm@gmail.com>
2014-08-28 18:29:14 -0500

    Edify: Add/Update abort messages for some asserts
    * If a bootloader or baseband assert is not satisfied, print the
      versions that are supported by the package as well as the version
      on the device.
    * Changes the layout of the device assert in the edify script with some
      more line breaks.
    * Cleanup the device abort message and remove extra quoting.
    
    Change-Id: I958d49281c51bd4e60d596a727bb94cfc4a21909

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -93,18 +93,21 @@ class EdifyGenerator(object):
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
     cmd = ('assert(' +
-           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+           ' || \0'.join(['getprop("ro.product.device") == "%s" ||\0getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) +
-           ' || abort("This package is for \\"%s\\" devices; '
-           'this is a \\"" + getprop("ro.product.device") + "\\".");'
+           ' ||\0abort("This package is for device: %s; ' +
+           'this device is " + getprop("ro.product.device") + ".");' +
            ');') % device
     self.script.append(self._WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
-    """Asert that the bootloader version is one of *bootloaders."""
+    """Assert that the bootloader version is one of *bootloaders."""
     cmd = ("assert(" +
            " ||\0".join(['getprop("ro.bootloader") == "%s"' % (b,)
                          for b in bootloaders]) +
+           ' ||\0abort("This package supports bootloader(s): ' +
+           ", ".join(["%s" % (b,) for b in bootloaders]) +
+           '; this device has bootloader: " + getprop("ro.bootloader") + ".");' +
            ");")
     self.script.append(self._WordWrap(cmd))
 
@@ -113,6 +116,9 @@ class EdifyGenerator(object):
     cmd = ("assert(" +
            " ||\0".join(['getprop("ro.baseband") == "%s"' % (b,)
                          for b in basebands]) +
+           ' ||\0abort("This package supports baseband(s): ' +
+           ", ".join(["%s" % (b,) for b in basebands]) +
+           '; this device has baseband: " + getprop("ro.baseband") + ".");' +
            ");")
     self.script.append(self._WordWrap(cmd))
 
