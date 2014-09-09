1a3550e07fae11ef19d1c63e9d449eba7a4e45c9
arter97 <qkrwngud825@gmail.com>
2014-09-09 11:44:22 +0900

    Apply correct, optimized mfpu compiler flag for ARMv7-A Cortex CPUs
    For those ARMv7-A Cortex CPUs that can handle VFPv4 floating point,
    We can set "-mfpu=neon-vfpv4" instead of generic "-mfpu=neon"
    to gain extra performance improvements.
    
    References :
     - GCC		: https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
     - Cortex A15	: http://www.arm.com/products/processors/cortex-a/cortex-a15.php
     - Cortex A9	: http://www.arm.com/products/processors/cortex-a/cortex-a9.php
     - Cortex A8	: http://www.arm.com/products/processors/cortex-a/cortex-a8.php
     - Cortex A7	: http://www.arm.com/products/processors/cortex-a/cortex-a7.php
     - Cortex A5	: http://www.arm.com/products/processors/cortex-a/cortex-a5.php
    
    Currently prebuilt clang doesn't support neon-vfpv4 flag so fall back to neon
    Also added support for cortex-a5 arch
    
    Change-Id: I91893789ed8edabf3767e1782e494b81158332bb
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Maxime Poulain <maxime.poulain@student.uclouvain.be>

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -40,5 +40,6 @@ CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
 define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
   $(subst -march=armv5e,-march=armv5,\
-  $(1)))
+  $(subst -mfpu=neon-vfpv4,-mfpu=neon,\
+  $(1))))
 endef
