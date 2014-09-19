9b2a79ec65b74e2c6e48bb73ecca25b4418b6c9b
Dan Pasanen <invisiblek@cyanogenmod.org>
2014-09-18 19:29:00 -0500

    repopick: support specifying a range of commits to pick
    * for example: repopick 12345-12350
    
    Change-Id: I3b8f2c331a6ac5488032432e133bd4b44d0bf007

--- tools/repopick.py
+++ tools/repopick.py
@@ -235,8 +235,16 @@ if __name__ == '__main__':
         reviews = fetch_query(args.gerrit, args.query)
         change_numbers = sorted([str(r['number']) for r in reviews])
     if args.change_number:
-        reviews = fetch_query(args.gerrit, ' OR '.join('change:{0}'.format(x.split('/')[0]) for x in args.change_number))
-        change_numbers = args.change_number
+        changelist = []
+        for c in args.change_number:
+            if '-' in c:
+                templist = c.split('-')
+                for i in range(int(templist[0]), int(templist[1]) + 1):
+                    changelist.append(str(i))
+            else:
+                changelist.append(c)
+        reviews = fetch_query(args.gerrit, ' OR '.join('change:{0}'.format(x.split('/')[0]) for x in changelist))
+        change_numbers = changelist
 
     # make list of things to actually merge
     mergables = []
