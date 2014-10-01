a6eec6f0f533e2722a79b2406a18831db7470bf6
Tom Marshall <tdm@cyngn.com>
2014-10-01 07:33:51 -0700

    build: Use project pathmap for recovery
    Change-Id: I6339ac77b899a43db21261d587252b65cb58ad79

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -41,7 +41,6 @@ pathmap_INCL := \
     libstdc++:bionic/libstdc++/include \
     mkbootimg:system/core/mkbootimg \
     opengl-tests-includes:frameworks/native/opengl/tests/include \
-    recovery:bootable/recovery \
     system-core:system/core/include \
     audio:system/media/audio/include \
     audio-effects:system/media/audio_effects/include \
