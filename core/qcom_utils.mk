e20fb65bd59039965e7557310df4a139493841c1
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-05 17:08:57 +0000

    qcom_utils: Make "is-vendor-board-platform,XX" depend on BOARD_HAS_XX_HARDWARE
    Prevent qc's code from wrongly kicking in for AOSP-derived
    configurations

--- core/qcom_utils.mk
+++ core/qcom_utils.mk
@@ -107,7 +107,7 @@ endef
 # $(call get-vendor-board-platforms,v)
 # returns list of board platforms for vendor v
 define get-vendor-board-platforms
-$($(1)_BOARD_PLATFORMS)
+$(if $(call match-word,$(BOARD_USES_$(1)_HARDWARE),true),$($(1)_BOARD_PLATFORMS))
 endef
 
 # $(call is-board-platform,bp)
