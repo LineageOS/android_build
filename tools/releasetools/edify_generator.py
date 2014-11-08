5c7635eddb8a8ca431d146e2e418a70c6282c0c4
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-08 23:03:53 +0000

    Remove nulls from the edify generator.
    These would break the scripts. WTF are they doing here?
    
    Change-Id: I0c52335a27f7ae974279fde1c2461b2dba24d263

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -133,7 +133,7 @@ class EdifyGenerator(object):
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
     cmd = ('assert(' +
-           ' || \0'.join(['getprop("ro.product.device") == "%s"' % i
+           ' || '.join(['getprop("ro.product.device") == "%s"' % i
                          for i in device.split(",")]) +
            ' || abort("E%d: This package is for device: %s; ' +
            'this device is " + getprop("ro.product.device") + ".");' +
@@ -143,7 +143,7 @@ class EdifyGenerator(object):
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
     cmd = ("assert(" +
-           " ||\0".join(['getprop("ro.bootloader") == "%s"' % (b,)
+           " || ".join(['getprop("ro.bootloader") == "%s"' % (b,)
                          for b in bootloaders]) +
            ");")
     self.script.append(self.WordWrap(cmd))
@@ -151,7 +151,7 @@ class EdifyGenerator(object):
   def AssertSomeBaseband(self, *basebands):
     """Assert that the baseband version is one of *basebands."""
     cmd = ("assert(" +
-           " ||\0".join(['getprop("ro.baseband") == "%s"' % (b,)
+           " || ".join(['getprop("ro.baseband") == "%s"' % (b,)
                          for b in basebands]) +
            ");")
     self.script.append(self._WordWrap(cmd))
