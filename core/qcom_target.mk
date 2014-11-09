1059484fad6c1f754194a0636eaea720676755ed
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:07:16 -0800

    build: Automatically set QCOM audio, display, media variants
    * Device platform should determing the HAL that ought to be used. This
      commit forces QCOM_HARDWARE to select the -caf HAL variants, which
      are then broken down by platform within the variant path.
    
    Change-Id: I6fc7a3def7b93112f034a3b89552f302727cdbf8

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -1,5 +1,10 @@
 # Target-specific configuration
 
+# Populate the qcom hardware variants in the project pathmap.
+define qcom-set-path-variant
+$(call project-set-path-variant,qcom-$(2),TARGET_QCOM_$(1)_VARIANT,hardware/qcom/$(2))
+endef
+
 # Enable DirectTrack on QCOM legacy boards
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
@@ -12,21 +17,22 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     endif
 
     # Enable DirectTrack for legacy targets
-    ifneq ($(filter caf bfam,$(TARGET_QCOM_AUDIO_VARIANT)),)
-        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
-            TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
-        endif
+    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+        TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
+        TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
     endif
-endif
 
-# Populate the qcom hardware variants in the project pathmap.
-define qcom-set-path-variant
-$(call project-set-path-variant,qcom-$(2),TARGET_QCOM_$(1)_VARIANT,hardware/qcom/$(2))
-endef
-$(call qcom-set-path-variant,AUDIO,audio)
+$(call project-set-path,qcom-audio,hardware/qcom/audio-caf)
 $(call qcom-set-path-variant,CAMERA,camera)
-$(call qcom-set-path-variant,DISPLAY,display)
+$(call project-set-path,qcom-display,hardware/qcom/display-caf)
 $(call qcom-set-path-variant,GPS,gps)
-$(call qcom-set-path-variant,MEDIA,media)
+$(call project-set-path,qcom-media,hardware/qcom/media-caf)
 $(call qcom-set-path-variant,SENSORS,sensors)
+else
+$(call project-set-path,qcom-audio,hardware/qcom/audio)
+$(call qcom-set-path-variant,CAMERA,camera)
+$(call project-set-path,qcom-display,hardware/qcom/display)
+$(call qcom-set-path-variant,GPS,gps)
+$(call project-set-path,qcom-media,hardware/qcom/media)
+$(call qcom-set-path-variant,SENSORS,sensors)
+endif
