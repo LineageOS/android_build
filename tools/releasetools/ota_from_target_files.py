40f5dcf52da58b6fce80cecb69b8776c292b134e
M1cha <sigmaepsilon92@gmail.com>
2014-11-25 15:30:48 +0100

    releasetools: ota_from_target_files: add FullOTA_PostValidate
    Change-Id: I152412049f90fd546d4516cc064238c3192be553

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -543,6 +543,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   common.CheckSize(boot_img.data, "boot.img", OPTIONS.info_dict)
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
 
+  device_specific.FullOTA_PostValidate()
+
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
     script.Mount("/system")
