26f47d0b945f2b564789f7b0368c4de16f0dd25c
M1cha <sigmaepsilon92@gmail.com>
2014-11-25 15:30:48 +0100

    releasetools: ota_from_target_files: add FullOTA_PostValidate
    Change-Id: I152412049f90fd546d4516cc064238c3192be553

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -545,6 +545,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   device_specific.FullOTA_PostValidate()
 
+  device_specific.FullOTA_PostValidate()
+
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
     if block_based:
