8c19d47be1c2d9f6b42f5db36da59fa3ff174952
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-11-30 06:12:35 +0100

    core: Enable -mcpu=cortex-a9 flag for Cortex-A9 cpu variant
    Change-Id: I9294a518bcdc21ccbae72eadd9f3c1a12982d028
    
    core: armv7-a-neon.mk: Add missing endif
    
    Change-Id: Id6f9c952d01d3c980115a52605d9c86038b3b5bd

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -19,6 +19,9 @@ ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VA
 	arch_variant_ldflags := \
 		-Wl,--no-fix-cortex-a8
 else
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
+	arch_variant_cflags := -mcpu=cortex-a9
+else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a8
 	arch_variant_ldflags := \
@@ -35,6 +38,7 @@ else
 	# Generic ARM might be a Cortex A8 -- better safe than sorry
 	arch_variant_ldflags := \
 		-Wl,--fix-cortex-a8
+endif
 endif
 endif
 endif
