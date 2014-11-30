a0c643d5916dd58d8d97851a9458c1e2fa9706bc
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-11-30 02:06:42 +0100

    core: Enable -mcpu=cortex-a8 flag for Scorpion cpu variant
    To be used with
    	http://review.cyanogenmod.org/#/c/77758/
    
    Change-Id: I7ecc4707fa45bd7098165615c0521a12c85fb087

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -9,7 +9,7 @@ ARCH_ARM_HAVE_NEON              := true
 ifneq (,$(filter cortex-a15 denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a15
 else
-ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
+ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
