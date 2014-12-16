8466daff04b941acb2cd0285c457a9f8396fe520
Ethan Chen <intervigil@gmail.com>
2014-12-15 18:45:19 -0800

    build: Add QCOM flags to Clang CFLAGS
    Change-Id: I56f0d4106f5d3d27c1ace744d30c1c81f0052bbd

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -25,6 +25,8 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,CAMERA,camera)
