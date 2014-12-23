e4442a2385e80df42dda9b17ffc15605f7f2c72f
Edward Wang <ewang@cyngn.com>
2014-12-23 15:57:43 -0800

    add arm64 compiler support
    Change-Id: I8f9e8c92b2cc42dab59d5b0b72df9060f15e01f4

--- envsetup.sh
+++ envsetup.sh
@@ -185,6 +185,12 @@ function setpaths()
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
             fi
             ;;
+        arm64)
+            toolchaindir=aarch64/aarch64-linux-android-$targetgccversion/bin;
+            if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
+                 export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
+            fi
+            ;;
         *)
             # No need to set ARM_EABI_TOOLCHAIN for other ARCHs
             ;;
