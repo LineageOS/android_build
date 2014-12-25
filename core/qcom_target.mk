a4b07c80b5199ad4194caf53bbe0c68aaeb13740
Steve Kondik <steve@cyngn.com>
2014-12-24 21:58:01 -0800

    build: Set QCOM global cflags for secondary arch
    Change-Id: I637d94f9b0ddee2c8d6057de44355e482163dd36

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -27,6 +27,12 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
+    # Multiarch needs these too..
+    2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,CAMERA,camera)
 $(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM))
