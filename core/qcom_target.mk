6c1f29c42bc30c314dae571fe4d4341e5ab083f3
Ricardo Cerqueira <ricardo@cyngn.com>
2015-01-02 01:55:49 +0000

    build: qcom: Set the correct project path for device-specific cameras
    Change-Id: I37696dae2c05c4e9f4fc824fbba63af47483ea17

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -37,7 +37,11 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
+ifeq ($(USE_DEVICE_SPECIFIC_CAMERA),true)
+$(call project-set-path,qcom-camera,$(TARGET_DEVICE_DIR)/camera)
+else
 $(call qcom-set-path-variant,CAMERA,camera)
+endif
 $(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,GPS,gps)
 $(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM))
