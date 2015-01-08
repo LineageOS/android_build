ab0b2ca6a0d0d24131bcb779f1eb882bf97afd3c
Brint E. Kriebel <bekit@cyngn.com>
2015-01-08 14:36:45 -0800

    releasetools: Store the build.prop file in the OTA zip
    This file is often used to read information about the update contained
    in the OTA. Place it in the update so it can be used by scripts. Add
    build.prop to incrementals too
    
    The file is not added to the updater-script, so it will not be placed
    onto the actual system.
    
    Change-Id: I88044796cbe8f199ca02df2840fd944cba2c73fa

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -220,7 +220,7 @@ OPTIONS.backuptool = False
 
 METADATA_NAME = 'META-INF/com/android/metadata'
 POSTINSTALL_CONFIG = 'META/postinstall_config.txt'
-UNZIP_PATTERN = ['IMAGES/*', 'META/*', 'INSTALL/*']
+UNZIP_PATTERN = ['IMAGES/*', 'META/*', 'INSTALL/*', 'SYSTEM/build.prop']
 
 
 class BuildInfo(object):
@@ -913,6 +913,9 @@ endif;
   script.SetProgress(1)
   script.AddToZip(input_zip, output_zip, input_path=OPTIONS.updater_binary)
   metadata["ota-required-cache"] = str(script.required_cache)
+
+  common.ZipWriteStr(output_zip, "system/build.prop",
+                     ""+input_zip.read("SYSTEM/build.prop"))
 
   # We haven't written the metadata entry, which will be done in
   # FinalizeMetadata.
@@ -1633,6 +1636,9 @@ endif;
   else:
     script.AddToZip(target_zip, output_zip, input_path=OPTIONS.updater_binary)
   metadata["ota-required-cache"] = str(script.required_cache)
+
+  common.ZipWriteStr(output_zip, "system/build.prop",
+                     ""+target_zip.read("SYSTEM/build.prop"))
 
   # We haven't written the metadata entry yet, which will be handled in
   # FinalizeMetadata().
