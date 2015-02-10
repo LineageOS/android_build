36ed62ec9088db44b0f81b9c80553ad9ee3b7ad5
Ricardo Cerqueira <ricardo@cyngn.com>
2015-02-10 23:58:10 +0000

    Revert "Add handheld_core_hardware.xml to telephony base"
    Don't do this. handheld_core_hardware.xml includes feature declarations
    that aren't mandatory, particularly cameras and magnetometers, but also
    things like managed users. According to the CDD, these are optional.
    Go back to the pre-L mechanism of including the declarations per-device
    to make them accurate.
    
    This reverts commit dccce7bbe9673d4b8ce1a3559182767f926e1ff8.
    
    Change-Id: I6db55082295171b78fe595e783799ee54458c4fd

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -23,10 +23,10 @@ PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=true
 
+ifeq ($(LINEAGE_BUILD),)
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-ifeq ($(LINEAGE_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 endif
