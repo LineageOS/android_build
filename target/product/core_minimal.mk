ec0d162d5f1d3a41ec86000a5e671a5be2043ecf
Scott Mertz <scott@cyngn.com>
2015-02-17 02:43:12 -0800

    [2/3] CmHardwareService: add cm hardware jar to system server
    Change-Id: Ie6ecb4af88a20163aceb3c38b38e10af10f1720f

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -97,6 +97,7 @@ PRODUCT_BOOT_JARS := \
 
 # The order of PRODUCT_SYSTEM_SERVER_JARS matters.
 PRODUCT_SYSTEM_SERVER_JARS := \
+    org.cyanogenmod.hardware \
     services \
     ethernet-service \
     wifi-service
