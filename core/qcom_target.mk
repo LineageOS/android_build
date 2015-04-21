81e241916fbaccb909110b08554257a0dc005b90
Steve Kondik <steve@cyngn.com>
2015-04-21 11:50:36 -0700

    build: Add a couple more custom pathmap variables
    Change-Id: Ie44b9844c6d82dd73d1ea1f16b565f8bb06652c1

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -7,6 +7,12 @@ endef
 define ril-set-path-variant
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
 endef
+define gps-hal-set-path-variant
+$(call project-set-path-variant,gps-hal,TARGET_GPS_HAL_PATH,$(1))
+endef
+define loc-api-set-path-variant
+$(call project-set-path-variant,loc-api,TARGET_LOC_API_PATH,$(1))
+endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
@@ -47,6 +53,8 @@ $(call qcom-set-path-variant,GPS,gps)
 $(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,SENSORS,sensors)
 $(call ril-set-path-variant,ril)
+$(call loc-api-set-path-variant,vendor/qcom/opensource/location)
+$(call gps-hal-set-path-variant,hardware/qcom/gps)
 else
 $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
 $(call qcom-set-path-variant,CAMERA,camera)
@@ -55,4 +63,6 @@ $(call qcom-set-path-variant,GPS,gps)
 $(call project-set-path,qcom-media,hardware/qcom/media/default)
 $(call qcom-set-path-variant,SENSORS,sensors)
 $(call ril-set-path-variant,ril)
+$(call loc-api-set-path-variant,vendor/qcom/opensource/location)
+$(call gps-hal-set-path-variant,hardware/qcom/gps)
 endif
