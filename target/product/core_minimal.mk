66113b62f73866cfd447947ad424af6db226038b
Adnan Begovic <adnan@cyngn.com>
2015-04-27 10:48:47 -0700

    build/target: Include Lineage platform jars in system server.
    Change-Id: Iaeecc4b1d8722f543b36cab76081cd49130a9eea

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -133,6 +133,8 @@ endif
 
 # The order of PRODUCT_SYSTEM_SERVER_JARS matters.
 PRODUCT_SYSTEM_SERVER_JARS := \
+    org.lineageos.platform \
+    org.lineageos.hardware \
     services \
     ethernet-service \
     wifi-service \
