cdc3b87fc9edbad5674cedd9af7a8c0048c72e91
Adnan Begovic <adnan@cyngn.com>
2015-04-27 16:25:55 -0700

    build/core: Provide sepolicy inheritance from cmsdk.
    Change-Id: I3a24bac32f8d85096400a4f765a0d7277ac67b20

--- core/config.mk
+++ core/config.mk
@@ -638,6 +638,7 @@ endif
 ifneq ($(CM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
+$(eval include vendor/cmsdk/sepolicy/sepolicy.mk)
 $(eval include vendor/cm/sepolicy/sepolicy.mk)
 endif
 
