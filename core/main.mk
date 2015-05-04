eb018c518cba0806101f983de85c4fd41b2f432e
Matt Mower <mowerm@gmail.com>
2015-05-04 14:30:30 -0500

    build: Unset JAVA_TOOL_OPTIONS to read java version
    If env variable JAVA_TOOL_OPTIONS is set, it breaks the expected java
    version output. Exp:
    
     Picked up JAVA_TOOL_OPTIONS: -javaagent:/usr/share/java/jayatanaag.jar
     java version "1.7.0_79"
     OpenJDK Runtime Environment (IcedTea 2.5.5) (7u79-2.5.5-0ubuntu1)
     OpenJDK 64-Bit Server VM (build 24.79-b02, mixed mode)
    
    Unset JAVA_TOOL_OPTIONS for the purposes of reading java and javac
    versions. This was encountered on an Ubuntu 15.04 system.
    
    Change-Id: I288faaa18f8589e29cd436fc9568c1a879618c65

--- core/main.mk
+++ core/main.mk
@@ -143,8 +143,8 @@ $(warning ************************************************************)
 $(error Directory names containing spaces not supported)
 endif
 
-java_version_str := $(shell unset _JAVA_OPTIONS && java -version 2>&1)
-javac_version_str := $(shell unset _JAVA_OPTIONS && javac -version 2>&1)
+java_version_str := $(shell unset _JAVA_OPTIONS && unset JAVA_TOOL_OPTIONS && java -version 2>&1)
+javac_version_str := $(shell unset _JAVA_OPTIONS && unset JAVA_TOOL_OPTIONS && javac -version 2>&1)
 
 # Check for the correct version of java, should be 1.7 by
 # default, and 1.6 if LEGACY_USE_JAVA6 is set.
