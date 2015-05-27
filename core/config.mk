3b03ae3a3cfa51c03b8ca678de7ce65737387137
Adnan Begovic <adnan@cyngn.com>
2015-05-26 18:21:15 -0700

    build/core: Inherit vendor/* makefiles for check-api.
    Change-Id: I72d409075192b18c35068566c5687092d45be9b4

--- core/config.mk
+++ core/config.mk
@@ -896,6 +896,13 @@ ifneq ($(CM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/cm/sepolicy/sepolicy.mk)
+
+# Include any vendor specific config.mk file
+-include $(TOPDIR)vendor/*/build/core/config.mk
+
+# Include any vendor specific apicheck.mk file
+-include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
