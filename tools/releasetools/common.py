3a0a1cfa3af7a2ad2d15610d8a00e75c6f84f2f0
Steve Kondik <steve@cyngn.com>
2015-06-10 13:05:24 -0700

    releasetools: Add support for LZMA in blockimgdiff
     * Requires backports.lzma to be installed for Python2.
     * To enable, set WITH_LZMA_OTA to true in the build environment.
     * This results in significantly smaller OTA packages, which results
       in less $$$ being paid to deliver said OTAs. Unfortunately it is
       also significantly slower to decompress (parallelization may help).
    
    Change-Id: If4b8092718aa6623cfff101030eabd24cde8763c
    (cherry picked from commit e78b239cbe782454d6df0916dc51bbf35ede5572)
    Ticket: CYNGNOS-326

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -29,6 +29,11 @@ import threading
 import time
 import zipfile
 
+try:
+  from backports import lzma;
+except ImportError:
+  lzma = None
+
 import blockimgdiff
 from rangelib import *
 
@@ -1100,11 +1105,12 @@ def ComputeDifferences(diffs):
 
 
 class BlockDifference:
-  def __init__(self, partition, tgt, src=None, check_first_block=False):
+  def __init__(self, partition, tgt, src=None, check_first_block=False, use_lzma=False):
     self.tgt = tgt
     self.src = src
     self.partition = partition
     self.check_first_block = check_first_block
+    self.use_lzma = use_lzma
 
     version = 1
     if OPTIONS.info_dict:
@@ -1113,7 +1119,7 @@ class BlockDifference:
           OPTIONS.info_dict.get("blockimgdiff_versions", "1").split(","))
 
     b = blockimgdiff.BlockImageDiff(tgt, src, threads=OPTIONS.worker_threads,
-                                    version=version)
+                                    version=version, use_lzma=use_lzma)
     tmpdir = tempfile.mkdtemp()
     OPTIONS.tempfiles.append(tmpdir)
     self.path = os.path.join(tmpdir, partition)
@@ -1151,18 +1157,26 @@ class BlockDifference:
 
   def _WriteUpdate(self, script, output_zip):
     partition = self.partition
+    suffix = ".new.dat"
+
     with open(self.path + ".transfer.list", "rb") as f:
       ZipWriteStr(output_zip, partition + ".transfer.list", f.read())
-    with open(self.path + ".new.dat", "rb") as f:
-      ZipWriteStr(output_zip, partition + ".new.dat", f.read())
+    if lzma and self.use_lzma:
+      suffix += ".xz"
+      with open(self.path + suffix, "rb") as f:
+        ZipWriteStr(output_zip, partition + suffix, f.read(),
+                           compression=zipfile.ZIP_STORED)
+    else:
+      with open(self.path + suffix, "rb") as f:
+        ZipWriteStr(output_zip, partition + suffix, f.read())
     with open(self.path + ".patch.dat", "rb") as f:
       ZipWriteStr(output_zip, partition + ".patch.dat", f.read(),
                          compression=zipfile.ZIP_STORED)
 
     call = (('block_image_update("%s", '
              'package_extract_file("%s.transfer.list"), '
-             '"%s.new.dat", "%s.patch.dat");\n') %
-            (self.device, partition, partition, partition))
+             '"%s%s", "%s.patch.dat");\n') %
+            (self.device, partition, partition, suffix, partition))
     script.AppendExtra(script._WordWrap(call))
 
   def _CheckFirstBlock(self, script):
