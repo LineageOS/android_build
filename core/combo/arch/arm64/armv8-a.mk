65280d912ba1637f63dee31dab73580f18da3834
Rashed Abdel-Tawab <rashed@linux.com>
2015-07-12 20:25:14 -0400

    build: Automatically include a53 errata fixes on targets that need it
     * Automatically include the errata fixes on 64bit chips using a53 cores
     * Automatically include the errata fixes on msm899x and exynos7 octa
       even if a53 cores are not specified
     * Leave the TARGET_CPU_CORTEX_A53 flag in case a device that needs these
       fixes doesn't fit into the checks above.
    
    Change-Id: I86917fc70445fef442226e6128c361f836bae755

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,5 +1,23 @@
 arch_variant_cflags :=
 
+# If the chip uses a53 cores, enable the errata workarounds
+ifeq ($(TARGET_2ND_CPU_VARIANT),cortex-a53)
+    TARGET_CPU_CORTEX_A53 := true
+else
+# Enable the errata fixes for platforms that are known to need it
+# in case BoardConfig doesn't declare a53
+    # msm899x platforms need the errata workarounds
+    ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+        TARGET_CPU_CORTEX_A53 := true
+    endif
+    # 64bit exynos 7 octa need the errata workarounds
+    ifneq ($(filter exynos5433 exynos7410 exynos7420,$(TARGET_SOC)),)
+        TARGET_CPU_CORTEX_A53 := true
+    endif
+endif
+
+# Leave the flag so devices that need the workaround but don't fit in
+# either check above can still enable it.
 ifeq ($(TARGET_CPU_CORTEX_A53),true)
 arch_variant_ldflags := -Wl,--fix-cortex-a53-843419 \
                         -Wl,--fix-cortex-a53-835769
