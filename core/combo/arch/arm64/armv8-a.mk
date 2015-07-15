262f6ddcd70a4482bfdbb3c4ebd6b73f0f521178
Ricardo Cerqueira <ricardo@cyngn.com>
2015-07-15 16:34:58 +0100

    aarch64: Default to enabling Cortex-A53 erratum 835769 on devices with A53
    Unless TARGET_CPU_CORTEX_A53 is explicitly set, default it to "true"
    
    Change-Id: I457cdf6175d257609037976f9da3bd1223404e1e

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,5 +1,15 @@
 arch_variant_cflags :=
 
+ifdef TARGET_2ND_ARCH
+  ifneq (,$(filter $(TARGET_$(TARGET_2ND_ARCH_VAR_PREFIX)CPU_VARIANT),cortex-a53))
+    TARGET_CPU_CORTEX_A53 ?= true
+  endif
+else
+  ifneq (,$(filter $(TARGET_CPU_VARIANT),cortex-a53))
+    TARGET_CPU_CORTEX_A53 ?= true
+  endif
+endif
+
 ifeq ($(TARGET_CPU_CORTEX_A53),true)
 arch_variant_ldflags := -Wl,--fix-cortex-a53-843419 \
                         -Wl,--fix-cortex-a53-835769
