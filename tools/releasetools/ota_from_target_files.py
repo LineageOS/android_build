e964714a06c158b41f90e49c025607fa9455c781
Tao Bao <tbao@google.com>
2015-08-07 19:49:45 -0700

    Change the cache partition size check into warnings.
    For some old builds, we may not define cache partition size. Change the
    exception into a warning to make the script backward compatible.
    
    Change-Id: Ie94c7fbb1a9f3a7db3f16e8d845e493a534aac5b

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -143,9 +143,6 @@ OPTIONS.oem_source = None
 OPTIONS.fallback_to_full = True
 OPTIONS.full_radio = False
 OPTIONS.full_bootloader = False
-# Stash size cannot exceed cache_size * threshold.
-OPTIONS.cache_size = None
-OPTIONS.stash_threshold = 0.8
 OPTIONS.backuptool = False
 OPTIONS.override_device = 'auto'
 OPTIONS.override_prop = False
@@ -1716,7 +1713,7 @@ def main(argv):
 
     cache_size = OPTIONS.info_dict.get("cache_size", None)
     if cache_size is None:
-      raise RuntimeError("can't determine the cache partition size")
+      print "--- can't determine the cache partition size ---"
     OPTIONS.cache_size = cache_size
 
     if OPTIONS.incremental_source is None:
