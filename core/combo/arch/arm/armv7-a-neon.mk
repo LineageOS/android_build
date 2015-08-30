543afd861fe10c95d12ad7e05efcb9a2902c363e
Christopher N. Hesse <raymanfx@gmail.com>
2015-08-30 17:49:55 +0200

    armv7-a-neon: Enable Cortex A53 Errata 843419, 835769 ldflags
    Change-Id: I75899e19e01392968bb40135ea2d868b86320b74

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -54,3 +54,8 @@ endif
 
 arch_variant_cflags += \
     -mfloat-abi=softfp
+
+ifeq ($(TARGET_CPU_CORTEX_A53),true)
+arch_variant_ldflags := -Wl,--fix-cortex-a53-843419 \
+                        -Wl,--fix-cortex-a53-835769
+endif
