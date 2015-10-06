f6f591d166085c91ca14b75121f8b010de09d180
Khalid Zubair <kzubair@cyngn.com>
2015-10-06 11:00:55 -0700

    mka: allow mka to be run from anywhere in the tree
    The Android build system expects to be launched from the top of the
    tree. Invoking mka at any other level often has unintended consequence
    (e.g. targets the host ARCH because ARCH was not set).  The various
    build shortcuts like m and mm invoke Make from the top of the tree. mka
    now does the same.
    
    Change-Id: Id956cf04cbaf1d12d12549ca54e32b9c2ce1fc29

--- envsetup.sh
+++ envsetup.sh
@@ -2329,14 +2329,20 @@ function cmrebase() {
 }
 
 function mka() {
-    case `uname -s` in
-        Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-            ;;
-        *)
-            mk_timer schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
-            ;;
-    esac
+    local T=$(gettop)
+    if [ "$T" ]; then
+        case `uname -s` in
+            Darwin)
+                make -C $T -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+                ;;
+            *)
+                mk_timer schedtool -B -n 1 -e ionice -n 1 make -C $T -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+                ;;
+        esac
+
+    else
+        echo "Couldn't locate the top of the tree.  Try setting TOP."
+    fi
 }
 
 function cmka() {
