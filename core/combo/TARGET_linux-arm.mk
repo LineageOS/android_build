dcf71a79574ac23437ba62efa8b72d991490fab9
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-10-10 10:20:38 -0400

    Set targetlegacygccversion to 4.8 for ARM targets
    * aarch64 and x86 both have arm-eabi-4.9 toolchain available, however arm does not currently.
    * This causes the check for an arm-eabi-4.9 toolchain to fail, thus only leaving the arm-linux-androideabi- to exist.
    * Set the TARGET_LEGACY_GCC_VERSION to 4.8 until one is uploaded, so that the kernel can be compiled with arm-eabi-4.8
    
    Was updated to 4.9 here: https://github.com/CyanogenMod/android_build/commit/bf8346e90c5a6d5d809fa7166d50714b2b6bc30d
    Toolchain check here: https://github.com/CyanogenMod/android_build/blob/cm-13.0/envsetup.sh#L189
    
    Change-Id: I24bff10e392a6cdced1797870e523144e83d611d
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -39,6 +39,7 @@ $(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := 4.9
 
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
 $(combo_2nd_arch_prefix)TARGET_GCC_VERSION := 4.9
+$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := 4.8
 else
 $(combo_2nd_arch_prefix)TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
