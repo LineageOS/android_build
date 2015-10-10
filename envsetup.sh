fecd67de3dce5edf35de0a5b48c5f9d868f0147d
Adnan Begovic <adnan@cyngn.com>
2015-10-09 18:42:35 -0700

    build/arm: Only fallback to legacy toolchain if legacy gcc specified.
    Change-Id: Idc2c2603ac3ac6ba70c88968110ee2f2ec7ce899

--- envsetup.sh
+++ envsetup.sh
@@ -151,6 +151,7 @@ function setpaths()
     # defined in core/config.mk
     targetgccversion=$(get_build_var TARGET_GCC_VERSION)
     targetgccversion2=$(get_build_var 2ND_TARGET_GCC_VERSION)
+    targetlegacygccversion=$(get_build_var TARGET_LEGACY_GCC_VERSION)
     export TARGET_GCC_VERSION=$targetgccversion
 
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
@@ -186,9 +187,9 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-eabi-$targetgccversion/bin
-            if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
-                 export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
+            legacytoolchaindir=arm/arm-eabi-$targetlegacygccversion/bin
+            if [ -d "$gccprebuiltdir/$legacytoolchaindir" ]; then
+                 export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$legacytoolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
             fi
             ;;
