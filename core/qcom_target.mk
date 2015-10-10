4ad6ea6e88596f881f1866a560bd8e443ea38aff
Rashed Abdel-Tawab <rashed@linux.com>
2015-10-10 15:15:34 -0400

    qcom: Enable TARGET_COMPILE_WITH_MSM_KERNEL
    In the 6.0 HALs, CAF added the TARGET_COMPILE_WITH_MSM_KERNEL
    which is enabled when building AOSP with an in-line kernel.
    Since we only use in-line kernel builds, go ahead and enable
    it across the board for all Qualcomm devices
    
    Change-Id: I36bba34b7f1009b0776256cf0e0ce57e6c7377f4

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -22,6 +22,9 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     TARGET_USES_QCOM_BSP := true
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
+    # Tell HALs that we're compiling an AOSP build with an in-line kernel
+    TARGET_COMPILE_WITH_MSM_KERNEL := true
+
     # Enable DirectTrack for legacy targets
     ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
         ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
