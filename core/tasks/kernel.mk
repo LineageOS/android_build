6ddfbc3389808e43b5b4d33f54b73d24be7c7cf2
Roman Birg <roman@cyngn.com>
2015-10-12 14:42:47 -0700

    build: add common darwin elf.h
    Change-Id: I062acc3f5d14c09c654f7de5bb34d93ca987b697
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -207,7 +207,7 @@ define clean-module-folder
 endef
 
 ifeq ($(HOST_OS),darwin)
-  MAKE_FLAGS += C_INCLUDE_PATH=$(ANDROID_BUILD_TOP)/external/elfutils/src/libelf/
+  MAKE_FLAGS += C_INCLUDE_PATH=$(ANDROID_BUILD_TOP)/build/core/tasks/darwin-include/
 endif
 
 ifeq ($(TARGET_KERNEL_MODULES),)
