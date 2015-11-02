c7170309cc67cb1e42c5f262c4f60be00fd7646f
Michael Bestas <mikeioannina@gmail.com>
2015-11-02 02:10:26 +0200

    full_base: Fix mismerge
    * Remove property that was mismerged in b3b47f8ecdc8c2657b34d89c3a8f40796a16f1f2
    
    Change-Id: I18be52cc4a167f52150e56859f5139bda09d34e4

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -23,10 +23,6 @@ PRODUCT_PACKAGES := \
     libfwdlockengine \
     WAPPushManager
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
