55387af55fdf0cab8d84743a6079f77e14f85239
Ethan Chen <intervigil@gmail.com>
2015-11-04 11:59:23 -0800

    build: Don't load dt.img task if custom mkbootimg is used
    Change-Id: I7617554a8dc6f44ea0c6a713d834da4fe558caec

--- core/tasks/dt_image.mk
+++ core/tasks/dt_image.mk
@@ -1,6 +1,7 @@
 #----------------------------------------------------------------------
 # Generate device tree image (dt.img)
 #----------------------------------------------------------------------
+ifeq ($(strip $(BOARD_CUSTOM_BOOTIMG_MK)),)
 ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DT)),true)
 ifeq ($(strip $(BUILD_TINY_ANDROID)),true)
 include device/qcom/common/dtbtool/Android.mk
@@ -31,4 +32,5 @@ $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTIMAGE_TARGET)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_DTIMAGE_TARGET)
+endif
 endif
