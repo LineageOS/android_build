a69cf8b1a3feb8ed338098fe7591a7ba37228a45
Arne Coucheron <arco68@gmail.com>
2015-11-06 06:28:15 +0100

    build: qcom: Define legacy ALSA audio flag
    Change-Id: I83cf184f56e1293f6abaaac7849505260499c9af

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -32,6 +32,10 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
         # Enable legacy graphics functions
         qcom_flags += -DQCOM_BSP_LEGACY
+        # Enable legacy audio functions
+        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+            qcom_flags += -DLEGACY_ALSA_AUDIO
+        endif
     endif
 
     TARGET_GLOBAL_CFLAGS += $(qcom_flags)
