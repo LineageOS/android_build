8c8968f4b74f4267b7d15ce4feccd117cab61b02
Jessica Wagantall <jwagantall@cyngn.com>
2015-11-11 13:00:36 -0800

    build: Do not make recoveries or recoveryzip
    Change-Id: I5a599eb3ba3f3450d9dfeab390c527a586bcaa68

--- tools/device/mkvendor.sh
+++ tools/device/mkvendor.sh
@@ -111,5 +111,3 @@ popd
 echo Done!
 echo Use the following command to set up your build environment:
 echo '  'lunch cm_$DEVICE-eng
-echo And use the follwowing command to build a recovery:
-echo '  '. build/tools/device/makerecoveries.sh cm_$DEVICE-eng
