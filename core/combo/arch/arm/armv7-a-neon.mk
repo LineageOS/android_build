1da4b2c6c00a9cfddf109f3c528f3bea9a78358c
Pat Erley <perley@cyngn.com>
2015-11-24 13:26:28 -0800

    Revert "Apply correct, optimized mfpu compiler flag for ARMv7-A Cortex CPUs"
    This reverts commit 8dd9ea10c50a989561b4d4d4f7c55947e8c1b19d.
    
    Using the new mfpu values was causing cts failures on arches that are
    only 32-bit capable:
    
     public static native double
    	java.lang.StrictMath.tan(double): 0.4595594355798443:
    				expected:<0.4949001792881326>
    				 but was:<0.49490017928813257>
    
    Conflicts:
    	core/clang/arm.mk
    	core/combo/arch/arm/armv7-a-neon.mk
    
    CYNGNOS-1309
    
    Change-Id: I57bb063a33121d42f8facb05e7fdda3b384209bc

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,7 +7,7 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifneq (,$(filter cortex-a15 denver krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a15
 
 	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
 	# don't advertise.
@@ -16,26 +16,26 @@ ifneq (,$(filter cortex-a15 denver krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VA
 		-Wl,--no-fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a9
 	arch_variant_ldflags := \
 		-Wl,--no-fix-cortex-a8
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a8
 	arch_variant_ldflags := \
 		-Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a7
 	arch_variant_ldflags := \
 		-Wl,--no-fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a5)
-	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a7
 	arch_variant_ldflags := \
 		-Wl,--no-fix-cortex-a8
 else
-	arch_variant_cflags := -march=armv7-a -mfpu=neon
+	arch_variant_cflags := -march=armv7-a
 	# Generic ARM might be a Cortex A8 -- better safe than sorry
 	arch_variant_ldflags := \
 		-Wl,--fix-cortex-a8
@@ -46,4 +46,5 @@ endif
 endif
 
 arch_variant_cflags += \
-    -mfloat-abi=softfp
+    -mfloat-abi=softfp \
+    -mfpu=neon
