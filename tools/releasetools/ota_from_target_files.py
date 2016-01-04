7a9d77bf0db9a89c29a666610abdb769bc1a3b04
Anthony King <anthonydking@slimroms.net>
2016-01-04 16:17:43 -0600

    py3: ota_from_target_files.py: fix some other shit?
    Change-Id: I032097c463598d1246feb92c02567175a65f90fd

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -152,7 +152,7 @@ OPTIONS.override_prop = False
 def MostPopularKey(d, default):
   """Given a dict, return the key corresponding to the largest
   value.  Returns 'default' if the dict is empty."""
-  x = list(d.items())
+  x = [(v, k) for (k, v) in d.items()]
   if not x:
     return default
   x.sort()
