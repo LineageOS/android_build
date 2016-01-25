11de9fc6999bb0b77eafff1188de14447a6fee00
Ricardo Cerqueira <ricardo@cyngn.com>
2016-01-25 16:49:27 +0000

    binary: Allow specifying a path for generated proto headers
    protobuf compilation defaults to dropping the generated objects and
    headers onto a path derived from the origin source path. This poses
    a problem with some of our repositories since they're not placed in
    their original paths, and may include hard references to pb headers
    assuming the original directory.
    
    Typical use of this will be to place a
    
    LOCAL_PROTOC_OUTPUT := <original repo path>
    
    line in the relevant module block (for example, "hardware/ril" for our
    "hardware/ril-caf")
    
    Change-Id: Ib9b0cadd47e29a0d2e693b331cfc18e71b048ee5
    
    binary: Don't run protobuf header relocations if origin/target are the same
    
    Change-Id: I3746746bdf1526674f345d884e2eccef85e946ef
    
    build: Fix LOCAL_PROTOC_OUTPUT
    
     * This code was refactored in 7.1 adjust accordingly.
     * This is used by projects like ril-caf where the proto-c generated
       sources dir might not match the project path.
    
    Change-Id: I7a7d0bb7a397b02deb5406a591059a877ac2c6da

--- core/binary.mk
+++ core/binary.mk
@@ -866,13 +866,25 @@ my_proto_c_includes += $(proto_gen_dir)
 proto_generated_cpps := $(addprefix $(proto_gen_dir)/, \
     $(patsubst %.proto,%.pb$(my_proto_source_suffix),$(proto_sources_fullpath)))
 
+define copy-proto-files
+$(if $(PRIVATE_PROTOC_OUTPUT), \
+   $(if $(call streq,$(PRIVATE_PROTOC_INPUT),$(PRIVATE_PROTOC_OUTPUT)),, \
+   $(eval proto_generated_path := $(dir $(subst $(PRIVATE_PROTOC_INPUT),$(PRIVATE_PROTOC_OUTPUT),$@)))
+   @mkdir -p $(dir $(proto_generated_path))
+   @echo "Protobuf relocation: $(basename $@).h => $(proto_generated_path)"
+   @cp -f $(basename $@).h $(proto_generated_path) ),)
+endef
+
 # Ensure the transform-proto-to-cc rule is only defined once in multilib build.
 ifndef $(my_host)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_proto_defined
 $(proto_generated_cpps): PRIVATE_PROTO_INCLUDES := $(TOP)
 $(proto_generated_cpps): PRIVATE_PROTOC_FLAGS := $(LOCAL_PROTOC_FLAGS) $(my_protoc_flags)
+$(proto_generated_cpps): PRIVATE_PROTOC_OUTPUT := $(LOCAL_PROTOC_OUTPUT)
+$(proto_generated_cpps): PRIVATE_PROTOC_INPUT := $(LOCAL_PATH)
 $(proto_generated_cpps): PRIVATE_RENAME_CPP_EXT := $(my_rename_cpp_ext)
 $(proto_generated_cpps): $(proto_gen_dir)/%.pb$(my_proto_source_suffix): %.proto $(my_protoc_deps) $(PROTOC)
 	$(transform-proto-to-cc)
+	$(copy-proto-files)
 
 $(my_host)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_proto_defined := true
 endif
