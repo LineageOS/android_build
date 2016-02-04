cf9ed0946cdcb9bb0e94f1e788005a22768ea469
Artefvck 07 <artefvck07@gmail.com>
2016-02-04 09:37:15 -0800

    releasetool: add OSIP partitions specifics
    - Declare OSIP as a new partition type
    - Standard boot.img update is not possible for OSIP partitions
    We are simply removing it on AOSP handling, and let the board handle
    it in its own releasetool.py
    
    Change-Id: Ie19fc3f6a0ca42e2eda264904b20fceeddbeaf53

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1687,7 +1687,8 @@ PARTITION_TYPES = {
     "squashfs": "EMMC",
     "ext2": "EMMC",
     "ext3": "EMMC",
-    "vfat": "EMMC"
+    "vfat": "EMMC",
+    "osip": "OSIP"
 }
 
 def GetTypeAndDevice(mount_point, info):
