31ed6e0df7fce788a936cdfc03e6b7c92bd7c976
Scott Mertz <scott@cyngn.com>
2016-02-09 11:28:53 -0800

    prebuild: compress shared libraries in prebundled apks
    Prebundled APK shared libraries get installed like any
    other APK.  Compress these libraries in the APK to save
    space.
    
    Change-Id: If3f85b43e492dd42d2da89d3b3f0a1b1b44ccdbb

--- core/prebuilt_internal.mk
+++ core/prebuilt_internal.mk
@@ -211,7 +211,9 @@ $(built_module): PRIVATE_EMBEDDED_JNI_LIBS := $(embedded_prebuilt_jni_libs)
 
 $(built_module) : $(my_prebuilt_src_file) | $(ACP) $(ZIPALIGN) $(SIGNAPK_JAR)
 	$(transform-prebuilt-to-target)
+ifneq ($(LOCAL_MODULE_PATH),$(TARGET_OUT_VENDOR)/bundled-app)
 	$(uncompress-shared-libs)
+endif
 ifneq ($(LOCAL_CERTIFICATE),PRESIGNED)
 	@# Only strip out files if we can re-sign the package.
 ifdef LOCAL_DEX_PREOPT
