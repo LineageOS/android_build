a4a3d478954a00e48f749f24a821e91b048f4f10
Adnan Begovic <adnan@cyngn.com>
2016-02-09 18:04:37 -0800

    Revert "build: Fix external checkapi mechanism for marshmallow."
    This reverts commit 5a61a0dc099a3577bf96853191a1f1385d366b81.
    
    Change-Id: I714846b14122fd2f6f068ec0dd41ffb5b4b42bc0

--- core/config.mk
+++ core/config.mk
@@ -736,6 +736,9 @@ $(eval include vendor/cm/sepolicy/sepolicy.mk)
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
 
+# Include any vendor specific apicheck.mk file
+-include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
