375f0ebedadddddddaf55897221abc12d86d25c7
Roman Birg <roman@cyngn.com>
2016-03-01 10:27:51 -0800

    build: better compatibility between zsh shells
    Handle equals expansion in zsh
    
    http://www.zsh.org/mla/users/2011/msg00160.html
    
    Change-Id: I2b0cb23991aa88c3fa2c4dce00f7c3a673176e0a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- envsetup.sh
+++ envsetup.sh
@@ -962,7 +962,7 @@ function mmm()
         local DIR TO_CHOP
         local GET_INSTALL_PATH=
 
-        if [ "$(__detect_shell)" == "zsh" ]; then
+        if [ "$(__detect_shell)" = "zsh" ]; then
             set -lA DASH_ARGS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^-.*$/')
             set -lA DIRS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^[^-].*$/')
         else
@@ -1035,7 +1035,7 @@ function mmma()
   local T=$(gettop)
   local DRV=$(getdriver $T)
   if [ "$T" ]; then
-    if [ "$(__detect_shell)" == "zsh" ]; then
+    if [ "$(__detect_shell)" = "zsh" ]; then
         set -lA DASH_ARGS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^-.*$/')
         set -lA DIRS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^[^-].*$/')
     else
@@ -1913,7 +1913,7 @@ function makerecipe() {
 
   repo forall -c '
 
-  if [ "$REPO_REMOTE" == "github" ]
+  if [ "$REPO_REMOTE" = "github" ]
   then
     pwd
     cmremote
@@ -1924,7 +1924,7 @@ function makerecipe() {
 
 function cmgerrit() {
 
-    if [ "$(__detect_shell)" == "zsh" ]; then
+    if [ "$(__detect_shell)" = "zsh" ]; then
         # zsh does not define FUNCNAME, derive from funcstack
         local FUNCNAME=$funcstack[1]
     fi
@@ -2319,7 +2319,7 @@ function dopush()
         echo "Device Found."
     fi
 
-    if (adb shell getprop ro.cm.device | grep -q "$CM_BUILD") || [ "$FORCE_PUSH" == "true" ];
+    if (adb shell getprop ro.cm.device | grep -q "$CM_BUILD") || [ "$FORCE_PUSH" = "true" ];
     then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
