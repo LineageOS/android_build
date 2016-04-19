81913799d00423201e4e403257ca515464d9c095
Scott Mertz <scott@cyngn.com>
2016-04-19 15:24:41 -0700

    kernel: specify more precise dtb folder for stock qcom dtbTool
    Change-Id: If1cb9ac3a1b825f970ae39923a377e47515f6943

--- core/tasks/dt_image.mk
+++ core/tasks/dt_image.mk
@@ -23,7 +23,7 @@ ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
 possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
 else
 # Most specific paths must come first in possible_dtb_dirs
-possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
+possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/qcom $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
 endif
 dtb_dir = $(firstword $(wildcard $(possible_dtb_dirs)))
 
