04be3594ed40062441597e480ea49f5d441c2bdf
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-09 15:16:19 +0200

    ninja: build: dt: Move to in-build directory search
    * We can't depend on a $OUT variable with ninja
    
    Change-Id: I9ff050a986e7bb2774d1326f5b396d0afb7f33c1

--- core/tasks/dt_image.mk
+++ core/tasks/dt_image.mk
@@ -25,11 +25,16 @@ else
 # Most specific paths must come first in possible_dtb_dirs
 possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
 endif
-dtb_dir = $(firstword $(wildcard $(possible_dtb_dirs)))
 
 define build-dtimage-target
     $(call pretty,"Target dt image: $@")
-    $(hide) $(DTBTOOL) $(BOARD_DTBTOOL_ARGS) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(dtb_dir)
+    $(hide) for dir in $(possible_dtb_dirs); do \
+        if [ -d "$$dir" ]; then \
+            dtb_dir="$$dir"; \
+            break; \
+        fi; \
+    done; \
+    $(DTBTOOL) $(BOARD_DTBTOOL_ARGS) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ "$$dtb_dir";
     $(hide) chmod a+r $@
 endef
 
