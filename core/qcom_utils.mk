37abe50088b1510a71a1dfd749a0f15fbaa6cb63
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-05-11 21:22:57 -0300

    build: Readd msm7x27a to QCOM Targets
    Change-Id: I87db6ee43059c256d338babbdfe414030a06b53e
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- core/qcom_utils.mk
+++ core/qcom_utils.mk
@@ -1,5 +1,6 @@
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
+QCOM_BOARD_PLATFORMS += msm7x27a
 QCOM_BOARD_PLATFORMS += msm7x30
 QCOM_BOARD_PLATFORMS += msm8226
 QCOM_BOARD_PLATFORMS += msm8610
