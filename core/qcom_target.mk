4009b97a100a4a8005eff6ad4022f26eb09ccf6c
Ricardo Cerqueira <ricardo@cyngn.com>
2016-05-23 16:39:40 +0100

    qcom: Support 8937/8953
    Change-Id: I84b1807e88d5be1230eab64bd1275a0677ce510f

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -60,10 +60,14 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         ifneq ($(filter msm8909 msm8916,$(TARGET_BOARD_PLATFORM)),)
             QCOM_HARDWARE_VARIANT := msm8916
         else
+        ifneq ($(filter msm8953 msm8937,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8937
+        else
         ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
             QCOM_HARDWARE_VARIANT := msm8994
         else
             QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+        endif
         endif
         endif
         endif
