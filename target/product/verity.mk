a2fe444af86c7c180d2602a85d064e4b01d2c4ed
Ricardo Cerqueira <ricardo@cyngn.com>
2016-07-04 11:49:06 +0100

    verity: Allow overloading the key path from environment
    Change-Id: I8d24b2b1da48cbbd5951dda5a7c8bb2de143fe7f

--- target/product/verity.mk
+++ target/product/verity.mk
@@ -22,7 +22,7 @@ PRODUCT_SUPPORTS_VERITY := true
 # The dev key is used to sign boot and recovery images, and the verity
 # metadata table. Actual product deliverables will be re-signed by hand.
 # We expect this file to exist with the suffixes ".x509.pem" and ".pk8".
-PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
+PRODUCT_VERITY_SIGNING_KEY ?= build/target/product/security/verity
 
 PRODUCT_PACKAGES += \
         verity_key
