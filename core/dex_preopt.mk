d29678196f64ce958e38d479a85136ba73bea8de
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-09-05 18:17:44 -0500

    build: don't dex preopt by default on linux
    Change-Id: I7726cb87da576d1fcc59a4a3108e5a7ca2bf5304

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -25,19 +25,19 @@ SYSTEM_OTHER_ODEX_FILTER ?= app/% priv-app/%
 
 # The default values for pre-opting: always preopt PIC.
 # Conditional to building on linux, as dex2oat currently does not work on darwin.
-ifeq ($(HOST_OS),linux)
-  WITH_DEXPREOPT_PIC ?= true
-  WITH_DEXPREOPT ?= true
+#ifeq ($(HOST_OS),linux)
+#  WITH_DEXPREOPT_PIC ?= true
+#  WITH_DEXPREOPT ?= true
 # For an eng build only pre-opt the boot image. This gives reasonable performance and still
 # allows a simple workflow: building in frameworks/base and syncing.
-  ifeq (eng,$(TARGET_BUILD_VARIANT))
-    WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-  endif
+#  ifeq (eng,$(TARGET_BUILD_VARIANT))
+#    WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+#  endif
 # Add mini-debug-info to the boot classpath unless explicitly asked not to.
-  ifneq (false,$(WITH_DEXPREOPT_DEBUG_INFO))
-    PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
-  endif
-endif
+#  ifneq (false,$(WITH_DEXPREOPT_DEBUG_INFO))
+#    PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
+#  endif
+#endif
 
 GLOBAL_DEXPREOPT_FLAGS :=
 ifeq ($(WITH_DEXPREOPT_PIC),true)
