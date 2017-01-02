eb5373ed85a0e7813fc64e7f4df56fd8df88da5b
Matt Mower <mowerm@gmail.com>
2017-01-02 02:31:27 -0600

    envsetup: Tweak nested executed commands
    $(( causes some text editors to expect arithmetic is incoming and then
    syntax highlighting is thrown off for the rest of the script due to
    missing )). Include a space before the subshell command to help
    distinguish the nested command.
    
    Change-Id: I5bae8f32b3c70ac460681fcfde1df0febbe554fd

--- envsetup.sh
+++ envsetup.sh
@@ -134,11 +134,11 @@ function check_product()
 
     if (echo -n $1 | grep -q -e "^lineage_") ; then
         CM_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
-        export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
+        export BUILD_NUMBER=$( (date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     elif (echo -n $1 | grep -q -e "^cm_") ; then
         # Fall back to cm_<product>
         CM_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
-        export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
+        export BUILD_NUMBER=$( (date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
         CM_BUILD=
     fi
