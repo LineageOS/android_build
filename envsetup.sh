557a76dc6f6121d5810c635345db817766f96d3e
Matt Mower <mowerm@gmail.com>
2017-01-02 02:24:58 -0600

    envsetup: Combine nested else; if into elif in check_product()
    Change-Id: I2ea97b892d6e3d0beab2910ae88143b1c213608c

--- envsetup.sh
+++ envsetup.sh
@@ -135,14 +135,12 @@ function check_product()
     if (echo -n $1 | grep -q -e "^lineage_") ; then
         CM_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
         export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
-    else
+    elif (echo -n $1 | grep -q -e "^cm_") ; then
         # Fall back to cm_<product>
-        if (echo -n $1 | grep -q -e "^cm_") ; then
-            CM_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
-            export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
-        else
-            CM_BUILD=
-        fi
+        CM_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
+        export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
+    else
+        CM_BUILD=
     fi
     export CM_BUILD
 
