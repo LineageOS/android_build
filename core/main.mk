bb05c8fc87643e7594b53b0fc6a3217745a2b0a3
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-08 01:01:52 -0600

    core: move platform-specific helper macros to target makefiles
    * These (qcom specifically) are now needed sooner than we had
      previously been importing them. Include them within their
      <platform>_target.mk makefiles. This ensures they'll be
      imported before their macros are needed.
    
    Change-Id: I06c6ab66446e2f0b54c245cf6c2cf665b649e0c9

--- core/main.mk
+++ core/main.mk
@@ -105,12 +105,6 @@ include $(BUILD_SYSTEM)/config.mk
 # be generated correctly
 include $(BUILD_SYSTEM)/cleanbuild.mk
 
-# Bring in Qualcomm helper macros
-include $(BUILD_SYSTEM)/qcom_utils.mk
-
-# Bring in Mediatek helper macros too
-include $(BUILD_SYSTEM)/mtk_utils.mk
-
 # Include the google-specific config
 -include vendor/google/build/config.mk
 
--- core/mtk_target.mk
+++ core/mtk_target.mk
@@ -1,3 +1,6 @@
+# Bring in Mediatek helper macros
+include $(BUILD_SYSTEM)/mtk_utils.mk
+
 ifeq ($(BOARD_USES_MTK_HARDWARE),true)
     mtk_flags := -DMTK_HARDWARE
 
