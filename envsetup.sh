4d38ca777a6b2e3930aaa3c478546f2fb74ea12b
Matt Mower <mowerm@gmail.com>
2017-01-10 14:55:34 -0600

    Partial Revert "build: Rebrand to LineageOS"
    Leave intact the changes related to gerrit, github, and the wiki
    (except we don't know the page structure of the wiki yet, so simply
    point to wiki.lineageos.org instead of a full path). Otherwise, this
    is CyanogenMod, not LineageOS.
    
    This reverts commit 64fac0394bae8c8589d8a4567b8fcba3649dce87.
    
    Change-Id: I1d23f28f1e286de575a9cfd401a27658ad480e61

--- envsetup.sh
+++ envsetup.sh
@@ -680,7 +680,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var CM_VERSION)
-        ZIPFILE=lineage-$MODVERSION.zip
+        ZIPFILE=cm-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
