1a95207ec41e54e29544e1bf1c299b910e34d08e
Matt Mower <mowerm@gmail.com>
2017-02-06 13:43:41 -0600

    core: Output Install/Package paths to terminal
    Output the full path of modules and packages to the terminal for easy
    double-click copy-and-paste.
    
    Change-Id: Idbb69a9b89483708ca4df10b3b10b5ec579e244b

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -313,12 +313,12 @@ ifneq (true,$(LOCAL_UNINSTALLABLE_MODULE))
 $(LOCAL_INSTALLED_MODULE): PRIVATE_POST_INSTALL_CMD := $(LOCAL_POST_INSTALL_CMD)
 ifneq ($(LOCAL_ACP_UNAVAILABLE),true)
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE) | $(ACP)
-	@echo "Install: $@"
+	@echo "Install: $@" >&2
 	$(copy-file-to-new-target)
 	$(PRIVATE_POST_INSTALL_CMD)
 else
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE)
-	@echo "Install: $@"
+	@echo "Install: $@" >&2
 	$(copy-file-to-target-with-cp)
 endif
 
@@ -335,7 +335,7 @@ ifdef my_init_rc
 my_init_rc_src := $(LOCAL_PATH)/$(my_init_rc)
 my_init_rc_installed := $(TARGET_OUT$(partition_tag)_ETC)/init/$(notdir $(my_init_rc_src))
 $(my_init_rc_installed) : $(my_init_rc_src) | $(ACP)
-	@echo "Install: $@"
+	@echo "Install: $@" >&2
 	$(copy-file-to-new-target)
 
 $(my_register_name) : $(my_init_rc_installed)
