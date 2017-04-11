0b05450ed770f4b9cccdec329934ef59a4369222
Pengxuan Zheng <pzheng@codeaurora.org>
2017-04-10 17:26:55 -0700

    Add support for building with proprietary compiler
    Change-Id: Ib846d2a1bed7af56671726d8a7b79598d9db1259

--- core/definitions.mk
+++ core/definitions.mk
@@ -1233,7 +1233,8 @@ define transform-cpp-to-o-compiler-args
 	$(PRIVATE_CPPFLAGS) \
 	$(PRIVATE_DEBUG_CFLAGS) \
 	$(PRIVATE_CFLAGS_NO_OVERRIDE) \
-	$(PRIVATE_CPPFLAGS_NO_OVERRIDE)
+	$(PRIVATE_CPPFLAGS_NO_OVERRIDE) \
+	$(if $(findstring $(SDCLANG_PATH),$(PRIVATE_CXX)),$(SDCLANG_COMMON_FLAGS))
 endef
 
 define clang-tidy-cpp
@@ -1273,7 +1274,8 @@ define transform-c-or-s-to-o-compiler-args
 	    $(PRIVATE_TARGET_GLOBAL_CONLYFLAGS) \
 	    $(PRIVATE_ARM_CFLAGS) \
 	 ) \
-	 $(1)
+	 $(1) \
+	$(if $(findstring $(SDCLANG_PATH),$(PRIVATE_CXX)),$(SDCLANG_COMMON_FLAGS))
 endef
 
 define transform-c-to-o-compiler-args
