5a57329c8ba60c6e17fa6240f1a9ffa269a2f25d
Pengxuan Zheng <pzheng@codeaurora.org>
2017-04-28 15:49:24 -0700

    Control building shared libs, static libs and executables with SDLLVM LTO
    Change-Id: I92bd8bf222969802568a5825e9d5eac11b43fda7

--- core/static_library.mk
+++ core/static_library.mk
@@ -7,6 +7,10 @@ ifndef my_module_multilib
 my_module_multilib := both
 endif
 
+ifeq ($(LOCAL_SDCLANG),true)
+include $(SDCLANG_FLAG_DEFS)
+endif
+
 LOCAL_2ND_ARCH_VAR_PREFIX :=
 include $(BUILD_SYSTEM)/module_arch_supported.mk
 
@@ -33,6 +37,12 @@ endif
 LOCAL_2ND_ARCH_VAR_PREFIX :=
 
 endif # TARGET_2ND_ARCH
+
+ifeq ($(LOCAL_SDCLANG),true)
+ifeq ($(LOCAL_SDCLANG_LTO),true)
+include $(SDCLANG_LTO_DEFS)
+endif
+endif
 
 my_module_arch_supported :=
 
