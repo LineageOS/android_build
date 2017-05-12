582fc33294966661867d4edbaadcedd9234334c3
Dan Pasanen <dan.pasanen@gmail.com>
2017-05-12 07:42:28 -0500

    envsetup: donuts for everyone
    Change-Id: I67b87317c205ac8bd16d93f0811bfec76500edcc

--- envsetup.sh
+++ envsetup.sh
@@ -1626,7 +1626,7 @@ function mk_timer()
     if [ $ret -eq 0 ] ; then
         echo -n "${color_success}#### make completed successfully "
     else
-        echo -n "${color_failed}#### make failed to build some targets "
+        echo -n "🍩🍩🍩🍩${color_failed} make failed to build some targets ${color_reset}🍩🍩🍩🍩"
     fi
     if [ $hours -gt 0 ] ; then
         printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
