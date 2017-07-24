9f83d4045fb3b7e60df9363bcd5d025c53cea703
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:05:30 +0200

    soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS
     * Soong product variable 'needs_text_relocations'
    
    Change-Id: Ib7ffb237085ca4b2e4280a459a5a622a6a3110b9
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -71,6 +71,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo ''; \
 	echo '    "ArtUseReadBarrier": $(if $(filter false,$(PRODUCT_ART_USE_READ_BARRIER)),false,true),'; \
 	echo ''; \
