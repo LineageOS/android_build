c3040f55cbed5fb1b7e2fb6ea195ac7b6084c31b
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Lineage build support
    core: Move lineage sepolicy to device/
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,16 +179,22 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A Lineage build needs only the Lineage product makefiles.
+ifneq ($(LINEAGE_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
 else
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
-endif
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+  else
+  # Read in all of the product definitions specified by the AndroidProducts.mk
+  # files in the tree.
+  all_product_configs := $(get-all-product-makefiles)
+  endif # TARGET_BUILD_APPS
+endif # LINEAGE_BUILD
 
+ifeq ($(LINEAGE_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
@@ -214,6 +220,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
