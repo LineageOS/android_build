fed70ccdf7caa6dec17d117490f5188600b1ae24
Simon Shields <simon@lineageos.org>
2017-08-29 23:03:29 +1000

    edify: bring back SetPermissionsRecursive
    * we still need this, even with block-based OTAs.
    
    This partially reverts commit f388104eaacd05cfa075d6478369e1d0df5ddbf3
    
    includes the following commit:
    
    commit d810b09316cb1abc55b9d44ac008a4245ba642fe
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Fri Sep 1 00:12:29 2017 +0200
    
        edify: Don't append capabilities if path starts with /tmp
    
        * tmpfs does not support xattrs.
    
        Change-Id: I90ce6455e3da91d3018f2f2a427e398281e654ab
    
    Change-Id: I4943e2e89ee5c810a63746c570dc5e31e95b8c53

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -318,6 +318,25 @@ class EdifyGenerator(object):
             target=target, source=source, patch=patch,
             code=common.ErrorCode.APPLY_PATCH_FAILURE)))
 
+  def SetPermissionsRecursive(self, fn, uid, gid, dmode, fmode, selabel,
+                              capabilities):
+    """Recursively set path ownership and permissions."""
+    if not self.info.get("use_set_metadata", False):
+      self.script.append('set_perm_recursive(%d, %d, 0%o, 0%o, "%s");'
+                         % (uid, gid, dmode, fmode, fn))
+    else:
+      if capabilities is None:
+        capabilities = "0x0"
+      cmd = 'set_metadata_recursive("%s", "uid", %d, "gid", %d, ' \
+          '"dmode", 0%o, "fmode", 0%o' \
+          % (fn, uid, gid, dmode, fmode)
+      if not fn.startswith("/tmp"):
+        cmd += ', "capabilities", "%s"' % capabilities
+      if selabel is not None:
+        cmd += ', "selabel", "%s"' % selabel
+      cmd += ');'
+      self.script.append(cmd)
+
   def WriteRawImage(self, mount_point, fn, mapfn=None):
     """Write the given package file into the partition for the given
     mount point."""
