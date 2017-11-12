4ae86f76a08118600a43792930ca746e47aba31d
Bruce Wayne <olivercscott@gmail.com>
2017-11-11 23:27:31 -0500

    core: config: Use host flex if requested
    Change-Id: If87b54c4eab002a52a64b774d5d8b8e15036dd01

--- core/config.mk
+++ core/config.mk
@@ -573,7 +573,11 @@ ZIPTIME := $(prebuilt_build_tools_bin)/ziptime
 # Generic tools.
 JACK := $(HOST_OUT_EXECUTABLES)/jack
 
+ifeq ($(USE_HOST_FLEX),yes)
+LEX := flex
+else
 LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
+endif
 # The default PKGDATADIR built in the prebuilt bison is a relative path
 # external/bison/data.
 # To run bison from elsewhere you need to set up enviromental variable
