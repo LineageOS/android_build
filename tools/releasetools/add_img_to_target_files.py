3c794bb85858ce77139721dea4563a5d43fc0e0c
Bowgo Tsai <bowgotsai@google.com>
2017-11-14 23:42:30 +0800

    Add support for /odm partition
    This CL is largely an adaptation of Change-Id
    I5d229f6ac729ea6df9ff1f14cee2e28972cd9b4d
    
    tools/vendor_buildinfo.sh is also renamed to tools/device_buildinfo.sh.
    The caller then can invoke device_buildinfo.sh "vendor" | "odm" to
    generate properties for vendor.img and odm.img, respectively.
    
    It adds the following variables:
    
    - BOARD_AVB_ODM_KEY_PATH
    - BOARD_AVB_ODM_ALGORITHM
    - BOARD_AVB_ODM_ROLLBACK_INDEX_LOCATION
    - BOARD_ODMIMAGE_FILE_SYSTEM_TYPE
    - BOARD_ODMIMAGE_EXTFS_INODE_COUNT
    - BOARD_ODMIMAGE_EXTFS_RSV_PCT
    - BOARD_ODMIMAGE_PARTITION_SIZE
    - BOARD_ODMIMAGE_JOURNAL_SIZE
    - BOARD_ODMIMAGE_SQUASHFS_BLOCK_SIZE
    - BOARD_ODMIMAGE_SQUASHFS_COMPRESSOR
    - BOARD_ODMIMAGE_SQUASHFS_COMPRESSOR_OPT
    - BOARD_ODMIMAGE_SQUASHFS_DISABLE_4K_ALIGN
    - BOARD_PREBUILT_ODMIMAGE
    - BOARD_USES_ODMIMAGE
    - LOCAL_ODM_MODULE
    - PRODUCT_ODM_BASE_FS_PATH
    - PRODUCT_ODM_VERITY_PARTITION
    - PRODUCT_ODM_PROPERTIES
    - TARGET_COPY_OUT_ODM
    - TARGET_OUT_ODM
    - TARGET_OUT_ODM_*
    
    Bug: 64195575
    Test: boot a Taimen with existing images
    
    Test: `make odmimage` with
          - BOARD_AVB_ENABLE := true
          - BOARD_ODMIMAGE_PARTITION_SIZE := 62914560
          - BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
          - TARGET_COPY_OUT_ODM := odm
          - PRODUCT_ODM_PROPERTIES += odm.test.build=success
    
    Test: `make odmimage` with
          - BOARD_ODMIMAGE_PARTITION_RESERVED_SIZE := 10485760
          - BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
          - BOARD_AVB_ENABLE := true
          - TARGET_COPY_OUT_ODM := odm
          - PRODUCT_ODM_PROPERTIES += odm.test.build=success
          - PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
    
    Change-Id: I4dea7b567ec49a766c7a4683decaf81c7e921d55
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- tools/releasetools/add_img_to_target_files.py
+++ tools/releasetools/add_img_to_target_files.py
@@ -74,7 +74,7 @@ OPTIONS.is_signing = False
 
 
 # Partitions that should have their care_map added to META/care_map.txt.
-PARTITIONS_WITH_CARE_MAP = ('system', 'vendor', 'product')
+PARTITIONS_WITH_CARE_MAP = ('system', 'vendor', 'product', 'odm')
 
 
 class OutputFile(object):
@@ -192,6 +192,22 @@ def AddProduct(output_zip):
       output_zip, OPTIONS.input_tmp, "IMAGES", "product.map")
   CreateImage(
       OPTIONS.input_tmp, OPTIONS.info_dict, "product", img,
+      block_list=block_list)
+  return img.name
+
+
+def AddOdm(output_zip):
+  """Turn the contents of ODM into an odm image and store it in output_zip."""
+
+  img = OutputFile(output_zip, OPTIONS.input_tmp, "IMAGES", "odm.img")
+  if os.path.exists(img.input_name):
+    print("odm.img already exists; no need to rebuild...")
+    return img.input_name
+
+  block_list = OutputFile(
+      output_zip, OPTIONS.input_tmp, "IMAGES", "odm.map")
+  CreateImage(
+      OPTIONS.input_tmp, OPTIONS.info_dict, "odm", img,
       block_list=block_list)
   return img.name
 
@@ -648,13 +664,19 @@ def AddImagesToTargetFiles(filename):
 
   has_recovery = OPTIONS.info_dict.get("no_recovery") != "true"
 
-  # {vendor,product}.img is unlike system.img or system_other.img. Because it
-  # could be built from source, or dropped into target_files.zip as a prebuilt
-  # blob. We consider either of them as {vendor,product}.img being available,
-  # which could be used when generating vbmeta.img for AVB.
+==== BASE ====
+  # {vendor,product,product-services}.img are unlike system.img or
+  # system_other.img. Because it could be built from source, or dropped into
+  # target_files.zip as a prebuilt blob. We consider either of them as
+  # {vendor,product,product-services}.img being available, which could be
+  # used when generating vbmeta.img for AVB.
+==== BASE ====
   has_vendor = (os.path.isdir(os.path.join(OPTIONS.input_tmp, "VENDOR")) or
                 os.path.exists(os.path.join(OPTIONS.input_tmp, "IMAGES",
                                             "vendor.img")))
+  has_odm = (os.path.isdir(os.path.join(OPTIONS.input_tmp, "ODM")) or
+                 os.path.exists(os.path.join(OPTIONS.input_tmp, "IMAGES",
+                                             "odm.img")))
   has_product = (os.path.isdir(os.path.join(OPTIONS.input_tmp, "PRODUCT")) or
                  os.path.exists(os.path.join(OPTIONS.input_tmp, "IMAGES",
                                              "product.img")))
@@ -734,6 +756,12 @@ def AddImagesToTargetFiles(filename):
     banner("product")
     partitions['product'] = AddProduct(output_zip)
 
+==== BASE ====
+  if has_product_services:
+    banner("product-services")
+    partitions['product-services'] = AddProductServices(output_zip)
+
+==== BASE ====
   if has_system_other:
     banner("system_other")
     AddSystemOther(output_zip)
