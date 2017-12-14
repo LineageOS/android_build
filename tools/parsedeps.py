329a696e59e74a66101d18f5d64ec7fb6be6052d
Technohacker <technohacker1995@gmail.com>
2017-12-14 16:42:46 +0530

    build: use env python2 in tools
    On hosts like Arch Linux, `env python` refers to Python 3
    instead of 2. This commit uses `env python2` to ensure that
    we use Python 2
    
    Change-Id: I1c110577ea90227eaf6c25b95f32153ad86e2636

--- tools/parsedeps.py
+++ tools/parsedeps.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # vim: ts=2 sw=2
 
 import optparse
--- tools/releasetools/sign_zip.py
+++ tools/releasetools/sign_zip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #
 # Copyright (C) 2017 The LineageOS Project
 #
