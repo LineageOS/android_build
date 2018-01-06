34c2711baafa9191045f674c3f69509307ce7735
Ethan Chen <intervigil@gmail.com>
2018-01-06 15:33:41 -0800

    HAX: Disable secure ADB
    Change-Id: Ie94a04a1dcb4ff2907e919d1ee8a633bf67666c1

--- core/main.mk
+++ core/main.mk
@@ -239,11 +239,11 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),user)
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
   endif
 
   ifeq ($(user_variant),userdebug)
