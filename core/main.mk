29ef9417915d9368d3e2b72398ed902f2e6bb97a
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-09 21:49:55 +0100

    Disable target debugging in userdebug
    Change-Id: I0fdd09a8b441d98407ed03b2849fdb23ca9ad7bb

--- core/main.mk
+++ core/main.mk
@@ -235,7 +235,8 @@ endif
 ## user/userdebug ##
 
 user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
-enable_target_debugging := true
+# Disable debugging in plain user/userdebug builds.
+enable_target_debugging := false
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
@@ -249,9 +250,6 @@ ifneq (,$(user_variant))
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
     tags_to_install += debug
-  else
-    # Disable debugging in plain user builds.
-    enable_target_debugging :=
   endif
 
   # Disallow mock locations by default for user builds
