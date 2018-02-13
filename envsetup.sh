595e1009df2c75f0e84c91a63914de4300491f11
Tim Schumacher <timschumi@gmx.de>
2018-02-13 10:35:18 +0100

    cmremote: Respect 'https' and 'www.' when determining the path
    Additionally, I squashed together the checks for
    git:// and http://
    
    Change-Id: Icfa2328a07d520376020770e3d98309aaef99a6b

--- envsetup.sh
+++ envsetup.sh
@@ -1490,15 +1490,11 @@ function cmremote()
     then
         echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
     fi
-    GERRIT_REMOTE=$(cat .git/config  | grep git://github.com | awk '{ print $NF }' | sed s#git://github.com/##g)
+    GERRIT_REMOTE=$(cat .git/config  | grep -E '(git|http(s)?)://(www\.)?github.com' | awk '{ print $NF }' | sed -r 's#(git|http(s)?)://(www\.)?github.com/##g')
     if [ -z "$GERRIT_REMOTE" ]
     then
-        GERRIT_REMOTE=$(cat .git/config  | grep http://github.com | awk '{ print $NF }' | sed s#http://github.com/##g)
-        if [ -z "$GERRIT_REMOTE" ]
-        then
-          echo Unable to set up the git remote, are you in the root of the repo?
-          return 0
-        fi
+        echo Unable to set up the git remote, are you in the root of the repo?
+        return 0
     fi
     CMUSER=`git config --get review.review.lineageos.org.username`
     if [ -z "$CMUSER" ]
