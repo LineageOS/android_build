70671e5df0e1592f760d75017ec5082692d50e0b
Simon Shields <simon@lineageos.org>
2018-03-02 12:55:47 +1100

    build: unconditionally use lineage pathmap
    even if we're not building a lineage target, we need to
    use the lineage pathmap to make things like recovery and ril
    happy
    
    Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -94,6 +94,4 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
-ifneq ($(LINEAGE_BUILD),)
 -include $(TOPDIR)vendor/lineage/build/core/pathmap.mk
-endif
