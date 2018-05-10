0c04881857de7fe5371e198a60fa6f527692f822
Uldiniad <olivercscott@gmail.com>
2018-05-10 12:34:15 -0400

    dex2oat: disable multithreading for WSL
    * In its current state, WSL does not support dex2oat multithreading
    
    PS5: Inline the thread manipulation based on HOST_OS
    
    Change-Id: I325d7f3428d74cb7a4845ef9cf339115a36ce832

--- core/dex_preopt_libart.mk
+++ core/dex_preopt_libart.mk
@@ -148,9 +148,13 @@ endif
 # $(1): the input .jar or .apk file
 # $(2): the output .odex file
 define dex2oat-one-file
+ifeq ($(HOST_OS_IS_LXSS),true)
+OPTIONAL_THREAD := "-j1"
+endif
 $(hide) rm -f $(2)
 $(hide) mkdir -p $(dir $(2))
 $(hide) ANDROID_LOG_TAGS="*:e" $(DEX2OAT) \
+	$(OPTIONAL_THREAD) \
 	--runtime-arg -Xms$(DEX2OAT_XMS) --runtime-arg -Xmx$(DEX2OAT_XMX) \
 	--class-loader-context=$(DEX2OAT_CLASS_LOADER_CONTEXT) \
 	--boot-image=$(PRIVATE_DEX_PREOPT_IMAGE_LOCATION) \
