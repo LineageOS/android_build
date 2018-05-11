502e01f65ef5ba625d38a26ff2be93b874e44c29
Uldiniad <olivercscott@gmail.com>
2018-05-11 12:44:56 -0400

    core: config: Use host ijar if requested
    Change-Id: I34de40bd0bb305d34f52b73c4256f4f5880f743b

--- core/config.mk
+++ core/config.mk
@@ -562,7 +562,13 @@ ACP := $(prebuilt_build_tools_bin)/acp
 CKATI := $(prebuilt_build_tools_bin)/ckati
 DEPMOD := $(HOST_OUT_EXECUTABLES)/depmod
 FILESLIST := $(SOONG_HOST_OUT_EXECUTABLES)/fileslist
+
+ifneq ($(IJAR_EXEC),)
+IJAR := $(IJAR_EXEC)
+else
 IJAR := $(prebuilt_build_tools_bin)/ijar
+endif
+
 MAKEPARALLEL := $(prebuilt_build_tools_bin)/makeparallel
 SOONG_JAVAC_WRAPPER := $(SOONG_HOST_OUT_EXECUTABLES)/soong_javac_wrapper
 SOONG_ZIP := $(SOONG_HOST_OUT_EXECUTABLES)/soong_zip
