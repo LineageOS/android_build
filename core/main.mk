ee6e2c8254a44f248c76669871694c23fbae0c71
Ethan Chen <intervigil@gmail.com>
2018-05-20 14:38:31 -0700

    build: Clean up makefile hook inclusions
    * Include MTK/QCOM rules directly from
      vendor/lineage/build/core/config.mk hook, since that is what it is
      there for.
    * Place main.mk hook into vendor/lineage/build/core/main.mk with the
      rest of the make build system hooks.
    
    Change-Id: I51bda9f5d35a63113d6eab98e06da8919ba46480

--- core/main.mk
+++ core/main.mk
@@ -327,7 +327,7 @@ ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.stack-trace-dir=/data/anr
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/lineage/config/main.mk
+-include vendor/lineage/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
