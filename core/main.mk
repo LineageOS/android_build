f567aa0fd05eb6707eebfabaaa9e96d6ae771e72
Ethan Chen <intervigil@gmail.com>
2018-05-20 14:38:31 -0700

    build: Clean up makefile hook inclusions
    * Include MTK/QCOM rules directly from
      vendor/lineage/build/core/config.mk hook, since that is what it is
      there for.
    * Remove apicheck.mk hook, this is unused, and if it turns out to be
      needed again, it can be included by the config.mk hook mentioned above.
    * Place main.mk hook into vendor/lineage/build/core/main.mk with the
      rest of the make build system hooks.
    
    Change-Id: I51bda9f5d35a63113d6eab98e06da8919ba46480

--- core/main.mk
+++ core/main.mk
@@ -239,11 +239,11 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),user)
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
   endif
 
   ifeq ($(user_variant),userdebug)
@@ -327,7 +327,7 @@ ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.stack-trace-dir=/data/anr
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/lineage/config/main.mk
+-include vendor/lineage/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
