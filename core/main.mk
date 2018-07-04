894bd690b0a714f61d788121fe7c988d1c4ccadb
Andreas Blaesius <andi@unlegacy-android.org>
2018-07-04 06:21:49 +0200

    Secure adb by default on user and userdebug
    Change-Id: I60785437b0dcbd60f1dc2a17bc3d8afdfd3e2272

--- core/main.mk
+++ core/main.mk
@@ -241,10 +241,7 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
-
-  ifeq ($(user_variant),user)
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
-  endif
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
