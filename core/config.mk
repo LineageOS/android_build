85b0c5f1306b2e305fd9f568a9c2b6b28511e3cc
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-07 08:22:55 -0700

    DO NOT MERGE: disable inclusion of Lineage sepol
    Change-Id: Ia1bf98c2b2c5844207bf57f263d40c7bb767f014

--- core/config.mk
+++ core/config.mk
@@ -1096,7 +1096,7 @@ endif
 ifneq ($(LINEAGE_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+#$(eval include device/lineage/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
