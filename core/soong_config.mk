836d2bc2b90d081b1ee0b8edde475023c10469dc
Simon Shields <simon@lineageos.org>
2018-08-10 14:39:32 +1000

    soong: include Lineage soong_config
    Change-Id: Ic7b8993f4a201f8a9d24c6ba32617fa34843afc2

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -175,6 +175,8 @@ $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call end_json_map))
 $(call end_json_map)
 
+include vendor/lineage/build/soong/soong_config.mk
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))
