3b4f58db9b856f6f70f9076287b261be37370379
Bruno Martins <bgcngm@gmail.com>
2018-09-26 21:50:02 +0100

    core_base: Remove libnfc_ndef from PRODUCT_PACKAGES
     * This was a dependency of legacy NXP NFC stack, deprecated long ago.
       The devices that are still using this stack are explicitly building
       libnfc, which in turn has libnfc_ndef as shared lib.
    
    Change-Id: Id3b859cdea00452f7d1ee21455e08fe342649a1e

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -29,7 +29,6 @@ PRODUCT_PACKAGES += \
     libfilterpack_imageproc \
     libgabi++ \
     libmdnssd \
-    libnfc_ndef \
     libpowermanager \
     libspeexresampler \
     libstagefright_soft_aacdec \
