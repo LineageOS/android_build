7f87da5ce22863c39a2ba1da5e036ebdb6ca9260
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-14 10:00:18 +0200

    Revert "build: Allow building disabled vbmeta images in signing process"
    * Superseeded
    
    This reverts commit dfad36f5fe6f350906a344871aebb3dc9cfac9ef.

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -768,7 +768,6 @@ def ImagePropFromGlobalDict(glob_dict, mount_point):
       "verity_fec",
       "verity_disable",
       "avb_enable",
-      "avb_disabled_vbmeta",
       "avb_avbtool",
       "avb_salt",
       "use_dynamic_partition_size",
