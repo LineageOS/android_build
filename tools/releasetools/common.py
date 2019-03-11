4f1e0342a595e0de3acb16d0f058bc4a62b56f99
Christopher N. Hesse <raymanfx@gmail.com>
2019-03-11 21:59:24 +0100

    releasetools: Conditionally apply lzma compressor
    LZMA_RAMDISK_TARGETS only affects generated images stored in
    out/target/product/<x>/ at the moment, but not those which are
    generated when an OTA package is built.
    
    Fix this behavior so devices that expect to have their ramdisks
    compressed can apply OTAs without breakage because of images being
    too large.
    
    Change-Id: Iab17215115c7b4887eac54f7bfbcd37444850ac7
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -361,7 +361,8 @@ def AppendAVBSigningArgs(cmd, partition):
 
 
 def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
-                        has_ramdisk=False, two_step_image=False):
+                        has_ramdisk=False, two_step_image=False,
+                        compressor="minigzip"):
   """Build a bootable image from the specified sourcedir.
 
   Take a kernel, cmdline, and optionally a ramdisk directory from the input (in
@@ -373,7 +374,7 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
   for building the requested image.
   """
 
-  def make_ramdisk():
+  def make_ramdisk(compressor):
     ramdisk_img = tempfile.NamedTemporaryFile()
 
     if os.access(fs_config_file, os.F_OK):
@@ -382,12 +383,12 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
     else:
       cmd = ["mkbootfs", os.path.join(sourcedir, "RAMDISK")]
     p1 = Run(cmd, stdout=subprocess.PIPE)
-    p2 = Run(["minigzip"], stdin=p1.stdout, stdout=ramdisk_img.file.fileno())
+    p2 = Run([compressor], stdin=p1.stdout, stdout=ramdisk_img.file.fileno())
 
     p2.wait()
     p1.wait()
     assert p1.returncode == 0, "mkbootfs of %s ramdisk failed" % (sourcedir,)
-    assert p2.returncode == 0, "minigzip of %s ramdisk failed" % (sourcedir,)
+    assert p2.returncode == 0, compressor + " of %s ramdisk failed" % (sourcedir,)
 
     return ramdisk_img
 
@@ -403,7 +404,7 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
   img = tempfile.NamedTemporaryFile()
 
   if has_ramdisk:
-    ramdisk_img = make_ramdisk()
+    ramdisk_img = make_ramdisk(compressor)
 
   # use MKBOOTIMG from environ, or "mkbootimg" if empty or not set
   mkbootimg = os.getenv('MKBOOTIMG') or "mkbootimg"
@@ -535,13 +536,15 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
 
 
 def GetBootableImage(name, prebuilt_name, unpack_dir, tree_subdir,
-                     info_dict=None, two_step_image=False):
+                     info_dict=None, two_step_image=False,
+                     compressor="minigzip"):
   """Return a File object with the desired bootable image.
 
   Look for it in 'unpack_dir'/BOOTABLE_IMAGES under the name 'prebuilt_name',
   otherwise look for it under 'unpack_dir'/IMAGES, otherwise construct it from
   the source files in 'unpack_dir'/'tree_subdir'."""
 
+  print("YADDA: name: " + name + ", compressor: " + compressor)
   prebuilt_path = os.path.join(unpack_dir, "BOOTABLE_IMAGES", prebuilt_name)
   if os.path.exists(prebuilt_path):
     print("using prebuilt %s from BOOTABLE_IMAGES..." % (prebuilt_name,))
@@ -567,7 +570,8 @@ def GetBootableImage(name, prebuilt_name, unpack_dir, tree_subdir,
   fs_config = "META/" + tree_subdir.lower() + "_filesystem_config.txt"
   data = _BuildBootableImage(os.path.join(unpack_dir, tree_subdir),
                              os.path.join(unpack_dir, fs_config),
-                             info_dict, has_ramdisk, two_step_image)
+                             info_dict, has_ramdisk, two_step_image,
+                             compressor)
   if data:
     return File(name, data)
   return None
