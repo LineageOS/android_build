60721530825896ff41f26656ca87042a662a7817
Dan Willemsen <dwillemsen@google.com>
2019-08-26 22:03:32 -0700

    Skip much of aux_config.mk
    This was showing up in the top-ten longest makefiles during the product
    config and cleanspec kati runs sometimes, even though it's really never
    used. So instead of running the sometimes-expensive find command, skip it
    unless an AUX OS was specified.
    
    At some point we should remove this entirely, but that's a larger change
    that doesn't need to happen yet.
    
    Test: build-aosp_arm.ninja is the same before/after
    Change-Id: I0ea10193d2f07e1717224e9020a7defaf6191c05

--- core/aux_config.mk
+++ core/aux_config.mk
@@ -151,6 +151,8 @@ AUX_ALL_SUBARCHS :=
 variant_sfx :=_aux_variant_config.mk
 os_sfx :=_aux_os_config.mk
 
+ifdef AUX_OS_VARIANT_LIST
+
 config_roots := $(wildcard device vendor)
 all_configs :=
 ifdef config_roots
@@ -181,6 +183,8 @@ $(foreach v,$(AUX_ALL_VARIANTS),\
   $(call aux-variant-validate,$(v)) \
 )
 endif
+
+endif # AUX_OS_VARIANT_LIST
 
 INSTALLED_AUX_TARGETS :=
 
