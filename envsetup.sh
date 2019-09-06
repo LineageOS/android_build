993f415a0a84b273f2cab7a2cd5faaa2d2977675
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-09-06 18:31:15 +0200

    envsetup: Automatically set CCACHE_EXEC to the system's ccache
    * check for ccache tool and set its path in CCACHE_EXEC if it hasn't been set already
    
    Change-Id: Ife9d0a723cb90b89392ee288ebe6e7e9f2be7eef

--- envsetup.sh
+++ envsetup.sh
@@ -1618,6 +1618,17 @@ validate_current_shell
 source_vendorsetup
 addcompletions
 
+# check and set ccache path on envsetup
+if [ -z ${CCACHE_EXEC} ]; then
+    ccache_path=$(which ccache)
+    if [ ! -z "$ccache_path" ]; then
+        export CCACHE_EXEC="$ccache_path"
+        echo "ccache found and CCACHE_EXEC has been set to : $ccache_path"
+    else
+        echo "ccache not found/installed!"
+    fi
+fi
+
 export ANDROID_BUILD_TOP=$(gettop)
 
 . $ANDROID_BUILD_TOP/vendor/lineage/build/envsetup.sh
