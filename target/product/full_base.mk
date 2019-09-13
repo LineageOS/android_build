bb9f331ad48e0cedb8eaf3ac68c1f28e5b9ab6be
Michael Bestas <mkbestas@lineageos.org>
2019-09-13 19:51:01 +0300

    build: Conditionally include AudioPackage14.mk instead of AllAudio.mk
    * AllAudio.mk doesn't contain latest material files and also
      contains multiple duplicate files. Just copy the latest
      material ones and we'll include the older files in vendor/lineage
    
    Change-Id: Ic7353216a3f3dc3c51006d951ac932cfd6e5f3b5

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -50,7 +50,11 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(LINEAGE_BUILD),)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+else
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
+endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
