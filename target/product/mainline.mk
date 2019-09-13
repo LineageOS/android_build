5a9e80081fa0b455d68f76ebda2d072f946d7bc9
Michael Bestas <mkbestas@lineageos.org>
2019-09-13 19:51:01 +0300

    build: Conditionally include AudioPackage14.mk instead of AllAudio.mk
    * AllAudio.mk doesn't contain latest material files and also
      contains multiple duplicate files. Just copy the latest
      material ones and we'll include the older files in vendor/lineage
    
    Change-Id: Ic7353216a3f3dc3c51006d951ac932cfd6e5f3b5

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -24,7 +24,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(LINEAGE_BUILD),)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+else
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
