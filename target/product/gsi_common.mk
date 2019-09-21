LineageOS/android_build

commit 0eb4c7fb1ec75b0b8ee2d15e7fc361dc8e0eede6

Fix inheritance of PRODUCT_PROPERTY_OVERRIDES for Lineage audio files
Otherwise the ones set in Lineage vendor will be overridden by these.

Change-Id: I3f87dfd009d8ddfd48972c15770599742b12c4af

Author: Abhisek Devkota <ciwrl@cyanogenmod.com>
Remove extra product property overrides

This appears to clash with vendor/lineage/common mk's for overrides; causes
a broken default alarm (and OnTheHunt is so bleh).

Change-Id: I5ec990d3c5ad56b573c45f1a3b20169c989851a0

target/product/gsi_common.mk
@@ -27,10 +27,12 @@ else
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
+ifeq ($(LINEAGE_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg \
+endif
 
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \

target/product/mainline.mk
@@ -30,8 +30,12 @@ else
 $(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
+ifeq ($(LINEAGE_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
 
 PRODUCT_PACKAGES += \

target/product/full_base.mk
@@ -41,10 +41,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+ifeq ($(LINEAGE_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US

target/product/handheld_system.mk
@@ -86,5 +86,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown \
+
+ifeq ($(LINEAGE_BUILD),)
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=OnTheHunt.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
+endif

commit 6d8615be177f36a7d336aa0b62e839f31e63c2e6

build: Fix endif being included in line continuations
* Move endif statements further down to prevent included in line
  continuations that otherwise cause build errors.

* Issue introduced by:
  https://github.com/LineageOS/android_build/commit/0eb4c7fb1ec75b0b8ee2d15e7fc361dc8e0eede6#diff-d88e43b99e72d05cdd3680e51126db0eR34

Change-Id: Iceac93077db424ff5145f41035be9beef0141583

target/product/gsi_common.mk
@@ -32,6 +32,7 @@ ifeq ($(LINEAGE_BUILD),)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg \
+
 endif
 
 # The mainline checking whitelist, should be clean up

target/product/mainline.mk
@@ -34,6 +34,7 @@ ifeq ($(LINEAGE_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.com.android.dataroaming=true \
+
 endif
 
 PRODUCT_PACKAGES += \