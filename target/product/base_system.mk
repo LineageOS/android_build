641145a72a4e97504772a14ffe96ee5e31df458e
Tri Vo <trong@google.com>
2019-09-24 15:03:05 -0700

    Remove ashmemd and related artifacts
    Bug: 139855428
    Test: n/a
    Change-Id: Ia0dcfc47d4792c7265a884b787a788ab80442a8e

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -33,7 +33,6 @@ PRODUCT_PACKAGES += \
     appops \
     app_process \
     appwidget \
-    ashmemd \
     atrace \
     audioserver \
     BackupRestoreConfirmation \
@@ -117,7 +116,6 @@ PRODUCT_PACKAGES += \
     libandroid_runtime \
     libandroid_servers \
     libartpalette-system \
-    libashmemd_client \
     libaudioeffect_jni \
     libbinder \
     libbinder_ndk \
