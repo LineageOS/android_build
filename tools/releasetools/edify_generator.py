5c969beef832441baa84be5913df8f7f756b4a11
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 18:45:17 +0200

    releasetools: Move system mount handling to backuptool
    * This allows the ROM to be flashed on any recovery, whether it mounts
      system to /system, /system_root or /mnt/system like Q recovery
    
    * Remove any type of system mount with static paths from updater-script
      to figure out the path dinamically through backuptool
    
    Change-Id: I7b33726305cb6f3eec146d26135f0081002a25dc

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -164,9 +164,11 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self.WordWrap(cmd))
 
-  def RunBackup(self, command, system_path):
-    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s", "%s");' % (
-        command, system_path)))
+  def RunBackup(self, command, mount_point):
+    if self.fstab:
+      p = self.fstab[mount_point]
+    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s", "%s", "%s");' % (
+        command, p.device, p.fs_type)))
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
