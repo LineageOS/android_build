6d695518d604ebda0f6aa5468958a8d472f0c06d
凛凛凛个鳖 <30337499+baolong24@users.noreply.github.com>
2020-01-03 15:34:47 +0800

    envsetup.sh: exTHmUI-ify

--- envsetup.sh
+++ envsetup.sh
@@ -138,11 +138,11 @@ function check_product()
         return
     fi
     if (echo -n $1 | grep -q -e "^lineage_") ; then
-        LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
+        EXTHMUI_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
     else
-        LINEAGE_BUILD=
+        EXTHMUI_BUILD=
     fi
-    export LINEAGE_BUILD
+    export EXTHMUI_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
