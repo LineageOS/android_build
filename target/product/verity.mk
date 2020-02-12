eabda2fb44b0204739b8064ed133cc170c48c2fc
Nolen Johnson <johnsonnolen@gmail.com>
2020-02-12 17:18:35 +0000

    Revert "Revert "fix "build: Only set the default verity key as a fallback"""
    This reverts commit 0613d3f75fd876ef7e8d6122527452c1742cbbea.
    
    Change-Id: I906c8a5e0f23f118ada9a939d00fc36bf92926df

--- target/product/verity.mk
+++ target/product/verity.mk
@@ -23,7 +23,9 @@ PRODUCT_SUPPORTS_VERITY_FEC := true
 # The dev key is used to sign boot and recovery images, and the verity
 # metadata table. Actual product deliverables will be re-signed by hand.
 # We expect this file to exist with the suffixes ".x509.pem" and ".pk8".
-PRODUCT_VERITY_SIGNING_KEY ?= build/target/product/security/verity
+ifeq ($(strip $(PRODUCT_VERITY_SIGNING_KEY)),)
+PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
+endif
 
 PRODUCT_PACKAGES += \
         verity_key
