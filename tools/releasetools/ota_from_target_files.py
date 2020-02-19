8a4793ffc46886dc25103f91e78f52f78842d919
LuK1337 <priv.luk@gmail.com>
2020-02-19 02:14:59 +0100

    releasetools: Use 0oXXX instead of 0XXX for octal
    * Fixes py3 syntax error.
    
    Change-Id: Ia9ca6e392f43694ddf4c952b07bf159e8dead36e

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -969,8 +969,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   CopyInstallTools(output_zip)
   script.UnpackPackageDir("install", "/tmp/install")
-  script.SetPermissionsRecursive("/tmp/install", 0, 0, 0755, 0644, None, None)
-  script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
+  script.SetPermissionsRecursive("/tmp/install", 0, 0, 0o755, 0o644, None, None)
+  script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0o755, 0o755, None, None)
 
   if target_info.get("system_root_image") == "true":
     sysmount = "/"
