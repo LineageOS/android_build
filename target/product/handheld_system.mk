9a7db7204bf54c15ebe90a6b7394b79caf97f248
Aaron Kling <webgeek1234@gmail.com>
2020-03-30 23:41:13 -0500

    Disable Traceur for non eng-builds
    Change-Id: I9cc1d747dc70bcf89486009f7ecae1628ccfc0aa

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -70,7 +70,6 @@ PRODUCT_PACKAGES += \
     Telecom \
     TelephonyProvider \
     TeleService \
-    Traceur \
     UserDictionaryProvider \
     VpnDialogs \
     vr \
@@ -96,3 +95,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.carrier?=unknown \
     ro.config.notification_sound?=OnTheHunt.ogg \
     ro.config.alarm_alert?=Alarm_Classic.ogg
+
+# Traceur for debug only
+PRODUCT_PACKAGES_ENG += \
+    Traceur
