9824c1e2b6c7995c197359d524a32c69d5138b56
merothh <merothh@gmail.com>
2020-08-16 06:16:37 +0530

    releasetools: mount /product before we run backuptool
    * Some GApps packages (eg. NikGApps) install to /product whereas others such as openGApps, dont install to but remove AOSP counterparts of the gapps being installed from /product.
    
    * Thereby mount /product so that backuptool can respect these changes on devices with a seperate partition.
    
    Change-Id: Ia15af4daa0ad8292d1b08ebb0bcbb287d73a9c9a

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1070,6 +1070,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     sysmount = "/system"
 
   if OPTIONS.backuptool:
+    if "/product" in target_info["fstab"]:
+      script.Mount("/product")
     script.RunBackup("backup", sysmount, target_info.get('use_dynamic_partitions') == "true")
 
   # All other partitions as well as the data wipe use 10% of the progress, and
@@ -1108,6 +1110,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
     script.RunBackup("restore", sysmount, target_info.get('use_dynamic_partitions') == "true")
+    if "/product" in target_info["fstab"]:
+      script.Unmount("/product")
 
   script.WriteRawImage("/boot", "boot.img")
 
