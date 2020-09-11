53540ab95a240171f790827ddbe973a735face54
LuK1337 <priv.luk@gmail.com>
2020-09-12 00:33:57 +0200

    sign_target_apks: Replace update-payload-key.pub.pem in RECOVERY as well
    Change-Id: Id0e5f59ae7b93ffa3135ac687b785f0ee2ebe607

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -513,6 +513,7 @@ def ProcessTargetFiles(input_tf_zip, output_tf_zip, misc_info,
             "BOOT/RAMDISK/system/etc/security/otacerts.zip",
             "BOOT/RAMDISK/system/etc/update_engine/update-payload-key.pub.pem",
             "RECOVERY/RAMDISK/system/etc/security/otacerts.zip",
+            "RECOVERY/RAMDISK/system/etc/update_engine/update-payload-key.pub.pem",
             "SYSTEM/etc/security/otacerts.zip",
             "SYSTEM/etc/update_engine/update-payload-key.pub.pem")):
       pass
@@ -789,6 +790,10 @@ def ReplaceOtaKeys(input_tf_zip, output_tf_zip, misc_info):
     common.ZipWriteStr(
         output_tf_zip,
         "BOOT/RAMDISK/system/etc/update_engine/update-payload-key.pub.pem",
+        pubkey)
+    common.ZipWriteStr(
+        output_tf_zip,
+        "RECOVERY/RAMDISK/system/etc/update_engine/update-payload-key.pub.pem",
         pubkey)
 
 
