f6b5ea8da6da0bc23b69051bbb4c271a113baafa
LuK1337 <priv.luk@gmail.com>
2020-09-11 15:34:56 +0200

    Unset BOARD_EXT4_SHARE_DUP_BLOCKS
    * This allows us to remount ext4 partitions read-write
      on devices with dynamic partitions.
    
    This reverts the following commits:
    
    Revert "Move BOARD_EXT4_SHARE_DUP_BLOCKS to BoardConfigMainlineCommon.mk"
    
    This reverts commit I0eb035ba6edf44719d5de4f09fd97b5783537597.
    
    Revert "enable BOARD_EXT4_SHARE_DUP_BLOCKS for generic_arm64"
    
    This reverts commit 125b6d6eeab8d9b2a343e9ab892fb0e241958b7a.
    
    Change-Id: I3768374d6da59f969ea44c1965f75c934edf3b57

--- target/board/BoardConfigEmuCommon.mk
+++ target/board/BoardConfigEmuCommon.mk
@@ -33,6 +33,8 @@ ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS),true)
   # emulator needs super.img
   BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT := true
 
+  BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
   # 4G + 8M
   BOARD_SUPER_PARTITION_SIZE := 4303355904
   BOARD_SUPER_PARTITION_GROUPS := emulator_dynamic_partitions
