34c6b9cd25e451edd8ce00eababe9289a42961a2
TheScarastic <warabhishek@gmail.com>
2020-09-12 17:28:03 +0000

    Don't make recovery patch for devices with prebuilt vendor
    Change-Id: I05c0155e6331bfeccd4ad545e003fa3fa3498044

--- tools/releasetools/make_recovery_patch.py
+++ tools/releasetools/make_recovery_patch.py
@@ -49,13 +49,19 @@ def main(argv):
 
   board_uses_vendorimage = OPTIONS.info_dict.get(
       "board_uses_vendorimage") == "true"
+  board_builds_vendorimage =  OPTIONS.info_dict.get(
+      "board_builds_vendorimage") == "true"
+  target_files_dir = None
 
-  if board_uses_vendorimage:
+  if board_builds_vendorimage:
     target_files_dir = "VENDOR"
-  else:
+  elif not board_uses_vendorimage:
     target_files_dir = "SYSTEM/vendor"
 
   def output_sink(fn, data):
+    if target_files_dir is None:
+      return
+
     with open(os.path.join(output_dir, target_files_dir,
                            *fn.split("/")), "wb") as f:
       f.write(data)
