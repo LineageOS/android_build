a92df075f2599b895cbd95860d4acefc60e86474
LuK1337 <priv.luk@gmail.com>
2022-02-11 17:54:04 +0100

    releasetools: Unconditionally store recovery.img in non-A/B OTA
    Change-Id: Ib5c9f2f0fa90fd921d8f61b74364a20aaecb842e

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -186,6 +186,8 @@ def WriteFullOTAPackage(input_zip, output_file):
 
   recovery_img = common.GetBootableImage("recovery.img", "recovery.img",
                                          OPTIONS.input_tmp, "RECOVERY")
+  common.CheckSize(recovery_img.data, "recovery.img", target_info)
+  common.ZipWriteStr(output_zip, "recovery.img", recovery_img.data)
   if OPTIONS.two_step:
     if not target_info.get("multistage_support"):
       assert False, "two-step packages not supported by this build"
@@ -193,7 +195,6 @@ def WriteFullOTAPackage(input_zip, output_file):
     assert fs.fs_type.upper() == "EMMC", \
         "two-step packages only supported on devices with EMMC /misc partitions"
     bcb_dev = {"bcb_dev": fs.device}
-    common.ZipWriteStr(output_zip, "recovery.img", recovery_img.data)
     script.AppendExtra("""
 if get_stage("%(bcb_dev)s") == "2/3" then
 """ % bcb_dev)
