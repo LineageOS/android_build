8793dfb0751977d39d9a4e74ef8e07e5ced14172
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-03 23:58:15 +0200

    releasetools: Remove A/B assertion on retrofit dynamic partition devices
    * Retrofit dynamic partitions works perfectly fine on non-A/B devices
    
    Change-Id: I0c361d7b3ff4905b9a73352dd3f3548b385fe685

--- tools/releasetools/check_partition_sizes.py
+++ tools/releasetools/check_partition_sizes.py
@@ -179,11 +179,8 @@ class DynamicPartitionSizeChecker(object):
       raise RuntimeError("check_partition_sizes should only be executed on "
                          "builds with dynamic partitions enabled")
 
-    # Retrofit dynamic partitions: 1 slot per "super", 2 "super"s on the device
+    # Retrofit dynamic partitions: 1 slot per "super"
     if dap == Dap.RDAP:
-      if slot != DeviceType.AB:
-        raise RuntimeError("Device with retrofit dynamic partitions must use "
-                           "regular (non-Virtual) A/B")
       return 1
 
     # Launch DAP: 1 super on the device
