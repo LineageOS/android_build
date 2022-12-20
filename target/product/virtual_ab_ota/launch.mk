6a7aafd6601d65b5983e76779616b8badb613e40
Michael Bestas <mkbestas@lineageos.org>
2022-12-20 22:42:00 +0200

    Add e2fsck/resize2fs/tune2fs to recovery/vendor_ramdisk
    Fixes: [ERROR]: recovery: [libfs_mgr]Unable to enable ext4 verity on
    /dev/block/by-name/metadata because /system/bin/tune2fs is missing
    
    in recovery.
    
    Test: e2fsck/resize2fs/tune2fs are present in ramdisks.
    Change-Id: I83223f48e4df8b89ff9b27b0912174360c053617

--- target/product/virtual_ab_ota/launch.mk
+++ target/product/virtual_ab_ota/launch.mk
@@ -18,4 +18,21 @@ PRODUCT_VIRTUAL_AB_OTA := true
 
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.enabled=true
 
-PRODUCT_PACKAGES += e2fsck_ramdisk
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    resize2fs_ramdisk \
+    tune2fs_ramdisk
+
+# For dedicated recovery partitions, we need to include fs tools
+# For GKI devices, BOARD_USES_RECOVERY_AS_BOOT is empty, but
+# so is BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT.
+ifdef BUILDING_RECOVERY_IMAGE
+ifneq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
+ifneq ($(BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT),true)
+PRODUCT_PACKAGES += \
+    e2fsck.recovery \
+    resize2fs.recovery \
+    tune2fs.recovery
+endif
+endif
+endif
