369dc6f37a0d4982557714ac5b1268407cd5b467
J0SH1X <aljoshua.hell@gmail.com>
2023-10-13 14:18:24 +0200

    Revert "Reland "Mark BOARD_BUILD_SYSTEM_ROOT_IMAGE as KATI_obsolete_var""
    This reverts commit 05c0807f2f55deeaadf30fd56940676fdddfaf7f.
    
    Change-Id: I20a478a41e631e2ae7e574a99eb0af7cb5eb84e5

--- core/config.mk
+++ core/config.mk
@@ -167,8 +167,6 @@ $(KATI_obsolete_var PRODUCT_SUPPORTS_BOOT_SIGNER,VB 1.0 and related variables ar
 $(KATI_obsolete_var PRODUCT_VERITY_SIGNING_KEY,VB 1.0 and related variables are no longer supported)
 $(KATI_obsolete_var BOARD_PREBUILT_PVMFWIMAGE,pvmfw.bin is now built in AOSP and custom versions are no longer supported)
 $(KATI_obsolete_var BUILDING_PVMFW_IMAGE,BUILDING_PVMFW_IMAGE is no longer used)
-$(KATI_obsolete_var BOARD_BUILD_SYSTEM_ROOT_IMAGE)
-
 # Used to force goals to build.  Only use for conditionally defined goals.
 .PHONY: FORCE
 FORCE:
@@ -938,6 +936,11 @@ ifeq ($(PRODUCT_RETROFIT_DYNAMIC_PARTITIONS),true)
           PRODUCT_RETROFIT_DYNAMIC_PARTITIONS)
     endif
   endif
+endif
+
+# TODO(b/241346584): Mark BOARD_BUILD_SYSTEM_ROOT_IMAGE as KATI_obsolete_var after all users are removed
+ifeq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE),true)
+    $(error BOARD_BUILD_SYSTEM_ROOT_IMAGE is deprecated)
 endif
 
 ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS),true)
