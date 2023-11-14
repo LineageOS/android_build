6bc01628bb9cead16ed54355656c7b9e1568c3da
Bruno Martins <bgcngm@gmail.com>
2023-11-14 14:02:35 +0000

    build: Add support for android-info-extra.txt
    This allows adding android-info-extra.txt to OTA packaging folder
    in case it exists in the target device tree, so that it can then
    be used in conjunction with custom releasetools. Historically,
    devices used to do assertions based on the modem version or even
    trusted zone version in order to prevent flashing builds if certains
    conditions were not met. That however, breaks `fastboot flashall`
    since it looks for the requirements set in board-info.txt.
    
    Change-Id: I029d778b44be4d9d877701d05124451052a4b73c

--- target/board/Android.mk
+++ target/board/Android.mk
@@ -35,6 +35,15 @@ endif
 
 $(call declare-0p-target,$(INSTALLED_ANDROID_INFO_TXT_TARGET))
 
+# Similarly to above, generate a file containing extra info
+# to be used only with custom releasetools
+INSTALLED_ANDROID_INFO_EXTRA_TXT_TARGET := $(PRODUCT_OUT)/android-info-extra.txt
+$(INSTALLED_ANDROID_INFO_EXTRA_TXT_TARGET): $(wildcard $(TARGET_DEVICE_DIR)/android-info-extra.txt)
+	$(call pretty,"Generated: ($@)")
+	$(hide) grep -v '#' $< > $@
+
+$(call declare-0p-target,$(INSTALLED_ANDROID_INFO_EXTRA_TXT_TARGET))
+
 # Copy compatibility metadata to the device.
 
 # Device Manifest
