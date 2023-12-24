fa527fa246dd26cf26dc2544aff2ac4afb318075
Michael Bestas <mkbestas@lineageos.org>
2023-12-25 00:03:50 +0200

    Replace device_kernel_headers with generated_kernel_headers
    Change-Id: I54a969d63f9644f89a231d52f2ef0919edd5b784

--- core/binary.mk
+++ core/binary.mk
@@ -64,6 +64,11 @@ endif
 ifneq (,$(findstring $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,$(LOCAL_ADDITIONAL_DEPENDENCIES)))
   LOCAL_ADDITIONAL_DEPENDENCIES := $(patsubst $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,,$(LOCAL_ADDITIONAL_DEPENDENCIES))
 endif
+
+# Replace device_kernel_headers with generated_kernel_headers
+ifneq (,$(findstring device_kernel_headers,$(LOCAL_HEADER_LIBRARIES)))
+  LOCAL_HEADER_LIBRARIES := $(patsubst device_kernel_headers,generated_kernel_headers,$(LOCAL_HEADER_LIBRARIES))
+endif
 endif
 
 # The following LOCAL_ variables will be modified in this file.
