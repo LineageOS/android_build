06f2a0910757c7250817d49021345500f89ae513
LuK1337 <priv.luk@gmail.com>
2024-11-05 11:29:43 +0100

    hax
    Change-Id: Ia2724ecae8307e7d363901949fabc51f8cad0040

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -207,8 +207,32 @@ logger = logging.getLogger(__name__)
 
 OPTIONS = common.OPTIONS
 
-OPTIONS.extra_apks = {}
-OPTIONS.extra_apex_payload_keys = {}
+OPTIONS.extra_apks = {
+  'com.android.bt.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.crashrecovery.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.contexthub.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.dumpstate.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.gatekeeper.nonsecure.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.power.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.thermal.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.threadnetwork.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.uwb.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.hardware.vibrator.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+  'com.android.uprobestats.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk',
+}
+OPTIONS.extra_apex_payload_keys = {
+  'com.android.bt.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.crashrecovery.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.contexthub.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.dumpstate.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.gatekeeper.nonsecure.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.power.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.thermal.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.threadnetwork.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.uwb.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.hardware.vibrator.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+  'com.android.uprobestats.apex': '/lineage/signing/signing-keys/com.android.appsearch.apk.pem',
+}
 OPTIONS.skip_apks_with_path_prefix = set()
 OPTIONS.key_map = {}
 OPTIONS.rebuild_recovery = False
@@ -1609,10 +1633,12 @@ def main(argv):
       names = names.split(",")
       for n in names:
         OPTIONS.extra_apks[n] = key
+        print('setting OPTIONS.extra_apks for', n, 'to', key)
     elif o == "--extra_apex_payload_key":
       apex_names, key = a.split("=")
       for name in apex_names.split(","):
         OPTIONS.extra_apex_payload_keys[name] = key
+        print('setting OPTIONS.extra_apex_payload_keys for', name, 'to', key)
     elif o == "--skip_apks_with_path_prefix":
       # Check the prefix, which must be in all upper case.
       prefix = a.split('/')[0]
