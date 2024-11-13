f7832304b624c3676eb4a94b0d4bf78d143bbe35
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-13 12:38:37 -0500

    straight up rekt
    Change-Id: I06dd2c827f5b133ed60434deb8c096fe7497cb11

--- core/main.mk
+++ core/main.mk
@@ -277,8 +277,8 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
-  ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+  ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=0
 
   ifeq ($(user_variant),user)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
