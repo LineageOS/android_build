1a645c5b94bce910221296f0963e9590c017fef1
Alexander Koskovich <akoskovich@pm.me>
2024-12-31 23:43:43 -0500

    Revert "releasetools: build_super_image: Do not set readonly attr"
    Breaks generating super image at flash time.
    
    This reverts commit a0e0926c5705e01a4f9a631e85a192de7d423879.
    
    Change-Id: Ib61f8083113731777fc94280203c66c17df3782d

--- tools/releasetools/build_super_image.py
+++ tools/releasetools/build_super_image.py
@@ -62,7 +62,7 @@ def GetArgumentsForImage(partition, group, image=None):
   image_size = sparse_img.GetImagePartitionSize(image) if image else 0
 
   cmd = ["--partition",
-         "{}:none:{}:{}".format(partition, image_size, group)]
+         "{}:readonly:{}:{}".format(partition, image_size, group)]
   if image:
     cmd += ["--image", "{}={}".format(partition, image)]
 
