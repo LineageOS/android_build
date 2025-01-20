421ba39b29b415b6749ff0564e8c072fe6d837ce
David Marble <drmarble1@gmail.com>
2025-01-20 21:37:23 +0000

    Revert "Install adb_keys into recovery"
    This reverts commit 062dc3788837ab1fea011e64570ebf9a0bbd34ad.
    
    Reason for revert: Patchset 1
    3:35 PM
    This causes a build error on gts4lvwifi, troika and marlin but not on bluejay.
    FAILED: out/target/product/troika/obj/PACKAGING/recovery_intermediates/ramdisk_files-timestamp
    could not make way for new symlink: root/product
    cannot delete non-empty directory: root/product
    rsync error: some files/attrs were not transferred (see previous errors) (code 23) at main.c(1338) [sender=3.2.7]
    
    Change-Id: I889916351e5fc8b01f09f5036c2e95d39d8398b8

--- target/product/security/Android.mk
+++ target/product/security/Android.mk
@@ -13,16 +13,5 @@ ifdef PRODUCT_ADB_KEYS
     LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/security
     LOCAL_PREBUILT_MODULE_FILE := $(PRODUCT_ADB_KEYS)
     include $(BUILD_PREBUILT)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := adb_keys.recovery
-    LOCAL_MODULE_STEM := adb_keys
-    LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
-    LOCAL_LICENSE_CONDITIONS := notice
-    LOCAL_NOTICE_FILE := build/soong/licenses/LICENSE
-    LOCAL_MODULE_CLASS := ETC
-    LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/product/etc/security
-    LOCAL_PREBUILT_MODULE_FILE := $(PRODUCT_ADB_KEYS)
-    include $(BUILD_PREBUILT)
   endif
 endif
