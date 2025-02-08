1767ef08b329a73a6db80d1c5f8e79acba1cabb0
Thomas Makin <halorocker89@gmail.com>
2025-02-08 19:48:03 +0000

    releasetools: remove scratch partition in ota
    * Terrible UX to not force replace adb remount changes on OTA.
    * Ensure consistent experience across version updates.
    
    * So, automatically remove scratch partition when
      possible (e.g. packages that use edify).
    * A/B devices sadly can't, so make life easier for some
      users at least.
    
    Change-Id: Iba5901f5f3073de3469287c036dcf48bc53cc525

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -4116,6 +4116,7 @@ class DynamicPartitionsDifference(object):
       comment('Remove all existing dynamic partitions and groups before '
               'applying full OTA')
       append('remove_all_groups')
+      append('remove scratch')
 
     for p, u in self._partition_updates.items():
       if u.src_group and not u.tgt_group:
