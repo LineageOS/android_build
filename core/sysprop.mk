882d7b475d5b880163ca205bfe29b56bc0a78af9
LuK1337 <priv.luk@gmail.com>
2025-02-19 18:22:16 +0100

    core: Treat vendor/lineage-priv/* as release-keys
    Change-Id: I506ee2b5374a398a4421321e1582ae6d4f10401e

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -174,6 +174,8 @@ endif
 # when the target-files is signed in a post-build step.
 ifeq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
 BUILD_KEYS := test-keys
+else ifneq ($(filter vendor/lineage-priv/%,$(DEFAULT_SYSTEM_DEV_CERTIFICATE)),)
+BUILD_KEYS := release-keys
 else
 BUILD_KEYS := dev-keys
 endif
