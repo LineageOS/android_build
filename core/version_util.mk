0b424e9d3063b2b861bf16bfe8326e8d1d5ce9d2
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-14 00:15:36 +0800

    [DNM] make: Use UTC timezone on date
    Change-Id: I08ef64be136edbec3704fe082141654c817a0194

--- core/version_util.mk
+++ core/version_util.mk
@@ -226,7 +226,7 @@ ifndef BUILD_DATETIME
   BUILD_DATETIME := $(shell date +%s)
 endif
 
-DATE := date -d @$(BUILD_DATETIME)
+DATE := TZ=UTC date -d @$(BUILD_DATETIME)
 .KATI_READONLY := DATE
 
 # Everything should be using BUILD_DATETIME_FROM_FILE instead.
