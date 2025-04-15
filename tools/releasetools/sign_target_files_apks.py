defb6c8a22f8e5e1bf4af30d35fd54fbcb06d116
Michael Bestas <mkbestas@gmail.com>
2025-04-15 18:56:13 +0300

    releasetools: Handle build description not ending in -keys
    New motorola devices have extra text after -keys
    
    Issue: calyxos#3172
    Change-Id: Ic6700b61be41f96be263c9c9363ecd050aa019eb

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -1186,8 +1186,10 @@ def RewriteProps(data):
         value = "/".join(pieces)
       elif key == "ro.build.description":
         pieces = value.split()
-        assert pieces[-1].endswith("-keys")
-        pieces[-1] = EditTags(pieces[-1])
+        for i in range(len(pieces) - 1, -1, -1):
+            if pieces[i].endswith("-keys"):
+                pieces[i] = EditTags(pieces[i])
+                break
         value = " ".join(pieces)
       elif key.startswith("ro.") and key.endswith(".build.tags"):
         value = EditTags(value)
