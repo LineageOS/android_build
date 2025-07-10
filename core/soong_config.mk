21eca07fb0c5f2d2958cc5e517c5da6a90b478d1
Nolen Johnson <johnsonnolen@gmail.com>
2025-07-09 23:33:34 -0400

    Revert "build: Squash of allowing a device to generically define its own headers"
    This reverts commit 339d20fdd65e773f5c88ea2c0341b9a7fdd8a869.
    
    Reason: Fix gen_picks
    Change-Id: Ibe606943e59cc467defacb5d98b98e05d72eb54f

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -150,7 +150,6 @@ $(call add_json_list, NativeCoverageExcludePaths,        $(NATIVE_COVERAGE_EXCLU
 $(call add_json_bool, ArtUseReadBarrier,                 $(call invert_bool,$(filter false,$(PRODUCT_ART_USE_READ_BARRIER))))
 $(call add_json_str,  BtConfigIncludeDir,                $(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR))
 $(call add_json_list, DeviceKernelHeaders,               $(TARGET_DEVICE_KERNEL_HEADERS) $(TARGET_BOARD_KERNEL_HEADERS) $(TARGET_PRODUCT_KERNEL_HEADERS))
-$(call add_json_str,  TargetSpecificHeaderPath,          $(TARGET_SPECIFIC_HEADER_PATH))
 $(call add_json_str,  VendorApiLevel,                    $(BOARD_API_LEVEL))
 $(call add_json_str,  VendorApiLevelPropOverride,        $(BOARD_API_LEVEL_PROP_OVERRIDE))
 $(call add_json_list, ExtraVndkVersions,                 $(PRODUCT_EXTRA_VNDK_VERSIONS))
