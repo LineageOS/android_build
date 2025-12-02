8e8ac9be8f3f5d0595e42b2cb470ab7b85b79906
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-01 23:52:47 -0500

    Revert "Remove Virtual A/B Retrofit support."
    This reverts commit f777366b83e37ec330c534ab7db22ab7b3b745c5.
    
    Reason: Several 4.14 and 4.19 shipping devices use this.
    Change-Id: If6b428bd7e0aa9f6f02be439ea831d5247ae01b0

--- tools/releasetools/check_partition_sizes.py
+++ tools/releasetools/check_partition_sizes.py
@@ -95,12 +95,15 @@ class Expression(object):
 class DeviceType(object):
   NONE = 0
   AB = 1
-  VAB = 2
+  RVAB = 2 # retrofit Virtual-A/B
+  VAB = 3
 
   @staticmethod
   def Get(info_dict):
     if info_dict.get("ab_update") != "true":
       return DeviceType.NONE
+    if info_dict.get("virtual_ab_retrofit") == "true":
+      return DeviceType.RVAB
     if info_dict.get("virtual_ab") == "true":
       return DeviceType.VAB
     return DeviceType.AB
@@ -192,6 +195,10 @@ class DynamicPartitionSizeChecker(object):
 
     # DAP + A/B: 2 slots in super
     if slot == DeviceType.AB:
+      return 2
+
+    # DAP + retrofit Virtual A/B: same as A/B
+    if slot == DeviceType.RVAB:
       return 2
 
     # DAP + Launch Virtual A/B: 1 *real* slot in super (2 virtual slots)
