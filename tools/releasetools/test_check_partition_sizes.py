bc90ce9ca7ad2c0fc6d81fcddd6f80270d930626
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-01 23:52:47 -0500

    Revert "Remove Virtual A/B Retrofit support."
    This reverts commit f777366b83e37ec330c534ab7db22ab7b3b745c5.
    
    Reason: Several 4.14 and 4.19 shipping devices use this.
    Change-Id: If6b428bd7e0aa9f6f02be439ea831d5247ae01b0

--- tools/releasetools/test_check_partition_sizes.py
+++ tools/releasetools/test_check_partition_sizes.py
@@ -94,6 +94,22 @@ class CheckPartitionSizesTest(test_utils.ReleaseToolsTestCase):
     with self.assertRaises(RuntimeError):
       CheckPartitionSizes(self.info_dict)
 
+  def test_retrofit_vab(self):
+    self.info_dict.update(common.LoadDictionaryFromLines("""
+        virtual_ab=true
+        virtual_ab_retrofit=true
+        """.split("\n")))
+    CheckPartitionSizes(self.info_dict)
+
+  def test_retrofit_vab_too_big(self):
+    self.info_dict.update(common.LoadDictionaryFromLines("""
+        virtual_ab=true
+        virtual_ab_retrofit=true
+        system_image_size=100
+        """.split("\n")))
+    with self.assertRaises(RuntimeError):
+      CheckPartitionSizes(self.info_dict)
+
   def test_vab(self):
     self.info_dict.update(common.LoadDictionaryFromLines("""
         virtual_ab=true
