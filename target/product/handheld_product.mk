77f428b012e603a9d19b719e6cb98ae7dcaca8db
AnierinB <anierin@evolution-x.org>
2026-09-22 05:18:49 +0000

    build: Move Contacts to telephony_product.mk
    Change-Id: Ieb8f97b148b37f101fbc111bd6c61d54c5f4ab87
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,7 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-    Contacts \
     DeskClock \
     preinstalled-packages-platform-handheld-product.xml \
     QuickSearchBox \
